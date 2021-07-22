0b4ec29e52ec07b81c504e97530c48c107feb627
iaa2021 <ivanovandrey45@gmail.com>
2021-07-22 20:40:03 +0300

    ce project

--- BPCE/BPCE.d
+++
@@ -1,4 +0,0 @@
-obj/BPCE.o: \
- /home/iaa/Desktop/VScodeProjects/C++/2021/Chapter12/BPCE/BPCE.cpp \
- /home/iaa/Desktop/VScodeProjects/C++/2021/Chapter12/BPCE/BPCE.h \
- /home/iaa/Desktop/VScodeProjects/C++/2021/Chapter12/BPCE/ComissionEmployee.h
--- BPCE/p771.d
+++
@@ -1,4 +0,0 @@
-obj/p771.o: \
- /home/iaa/Desktop/VScodeProjects/C++/2021/Chapter12/BPCE/p771.cpp \
- /home/iaa/Desktop/VScodeProjects/C++/2021/Chapter12/BPCE/BPCE.h \
- /home/iaa/Desktop/VScodeProjects/C++/2021/Chapter12/BPCE/ComissionEmployee.h
