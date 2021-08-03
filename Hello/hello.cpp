706ac4894ae83edbb871401a6a33919f06a0b7ae
Ivanov Andrey <ivanovandrey45@gmail.com>
2021-08-03 21:47:39 +0300

    first change to shared ptr

--- Hello/hello.cpp
+++ Hello/hello.cpp
@@ -19,6 +19,8 @@ int main()
 {
     shared_ptr<MyClass>shPtr1 = make_shared<MyClass>(); 
     cout << "Shared count: " << shPtr1.use_count() << endl;
+    shared_ptr<MyClass>shPtr2 = shPtr1; 
+    cout << "Shared count: " << shPtr1.use_count() << endl;
     getch();
     return 0;
     
