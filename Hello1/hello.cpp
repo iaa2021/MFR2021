9624cd6f6f0ade37eca8e9a3792f34c0580cc9a8
Ivanov Andrey <ivanovandrey45@gmail.com>
2021-08-05 20:21:26 +0300

    work with shared pointers

--- Hello1/hello.cpp
+++ Hello1/hello.cpp
@@ -2,13 +2,26 @@
 #include <conio.h>
 #include <memory>
 using namespace std;
-
+class MyClass   
+{
+    public:
+    MyClass()
+{
+    cout << "Constructor invoked.\n";
+}
+    ~MyClass()
+    {
+        cout << "Destructor invoked.\n";
+    }
+    
+};
 
 int main()
 {
-    unique_ptr<int>unPtr1 = make_unique<int>(25);
-    unique_ptr<int>unPtr2 = move( unPtr1 );
-    cout << *unPtr2 << endl; 
+    shared_ptr<MyClass>shPtr1 = make_shared<MyClass>();
+    cout << "Shared count: " << shPtr1.use_count() << endl; 
+    shared_ptr<MyClass>shPtr2 = shPtr1;
+    cout << "Shared count: " << shPtr1.use_count() << endl;
     getch();
     return 0;
     
