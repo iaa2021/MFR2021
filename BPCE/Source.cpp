2b454572ce9a355742e3d27668f4c983cc9cfbbf
Ivanov Andrey <ivanovandrey45@gmail.com>
2021-08-13 20:10:58 +0300

    correcting BPCE 18.08.2021

--- BPCE/Source.cpp
+++ BPCE/Source.cpp
@@ -1,3 +1,7 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -8,10 +12,12 @@ int main()
     ComissionEmployee ce("Sue", "Jones", "222-22-2222", 10000, 0.6);
     BPCE employee("Bob", "Lewis", "333-33-3333", 5000, 0.4, 300);
     cout << fixed << setprecision(2);
-    ComissionEmployee* CEptr = 0;
-    BPCE* BPCEptr = 0;
-    CEptr = &ce;
+    ComissionEmployee* CEptr = &ce;
+    BPCE* BPCEptr = &employee;
     cout << "Invoking print function by Ceptr:\n";
     CEptr->print();
+    cout << "\nInvoking print function by BPCeptr:\n";
+    BPCEptr->print();
+    cin.get();
     return 0;
 }
