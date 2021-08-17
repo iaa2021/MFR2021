f5044bfad239b398a2fc436adf72c5c6a1b15c12
Ivanov Andrey <ivanovandrey45@gmail.com>
2021-08-17 20:02:42 +0300

    work with unique_ptr with (new ) method

--- Hello1/Hello1/Hello1.cpp
+++ Hello1/Hello1/Hello1.cpp
@@ -22,12 +22,8 @@ class MyClass
 };
 int main()
 {
-    weak_ptr< int > wePtr1;
-    {
-        shared_ptr<int>shPtr1 = make_shared<int>( 25 );
-        wePtr1 = shPtr1;
-        
-    }
+    
+    unique_ptr<MyClass>(new MyClass);
     cin.get();
     return 0;
 }
