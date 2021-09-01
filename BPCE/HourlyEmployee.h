45f7ea81d6821fe0f372f58cbbfa0c39f4ef7dce
Ivanov Andrey <ivanovandrey45@gmail.com>
2021-09-01 19:33:19 +0300

    added Hourly and Salaried Employees

---
+++ BPCE/HourlyEmployee.h
@@ -0,0 +1,18 @@
+#ifndef SALARIEDEMPLOYEE_H
+#define SALARIEDEMPLOYEE_H
+#include "Employee.h"
+class HourlyEmployee : public Employee
+{
+public:
+	HourlyEmployee(const string&, const string&, const string&, double = 0.0, double = 0.0);
+    void setWage(double);
+    double getWage() const;
+    void setHours(double);
+    double getHours() const;
+    virtual double earnings() const;
+    virtual void print() const;
+private:
+    double wage;
+    double hours;
+};
+#endif
