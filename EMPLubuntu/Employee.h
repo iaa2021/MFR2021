2f6a1759e7ac505bb35b9f00813e5c8a0ddaa104
iaa2021 <ivanovandrey45@gmail.com>
2021-09-13 09:49:25 +0300

    adding default to virtual destructor of Employee

--- EMPLubuntu/Employee.h
+++ EMPLubuntu/Employee.h
@@ -6,7 +6,7 @@ class Employee
 {
 public:
     Employee(const string&, const string&, const string&);
-    virtual ~Employee();
+    virtual ~Employee() = default;
     void setFirstName(const string&);
     string getFirstName() const;
     void setLastName(const string&);
