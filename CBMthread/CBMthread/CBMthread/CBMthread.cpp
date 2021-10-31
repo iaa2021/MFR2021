e2093a7b2b6e50e7c777151bb65b006dd3e7ea47
Ivanov Andrey <ivanovandrey45@gmail.com>
2021-10-31 22:35:37 +0200

    work with CBMthread

--- CBMthread/CBMthread/CBMthread/CBMthread.cpp
+++ CBMthread/CBMthread/CBMthread/CBMthread.cpp
@@ -16,18 +16,19 @@ void refreshfMap(map <string, int> fMap)
             cout << item.first << " - " << item.second << endl;
         }
         this_thread::sleep_for(2000ms);
+        cout << endl;
     }
 }
 int main()
 {
     srand(time(0));
     string cities[] = { "New York", "Mumbai", "Berlin", "Oslo", "London", "Paris", "Bruselle" };
-    map <string, int> fMap1 =
+    map <string, int> fMap =
     {
         { "New York", 19 },
         { "Mumbai", 21 },
         { "Berlin", 20 }
-    };
+    }; 
     thread bgWorker(refreshfMap, fMap);
     cin.get();
 }
