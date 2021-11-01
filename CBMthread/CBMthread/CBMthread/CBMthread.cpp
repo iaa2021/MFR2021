c8fb2f690fb2934e75a38b9326c46046fbc97306
Ivanov Andrey <ivanovandrey45@gmail.com>
2021-11-01 11:37:16 +0200

    CBMT with map's way

--- CBMthread/CBMthread/CBMthread/CBMthread.cpp
+++ CBMthread/CBMthread/CBMthread/CBMthread.cpp
@@ -22,14 +22,19 @@ void refreshfMap(map <string, int> fMap)
 int main()
 {
     srand(time(0));
-    string cities[] = { "New York", "Mumbai", "Berlin", "Oslo", "London", "Paris", "Bruselle" };
+    string cities[] = { "New York", "Mumbai", "Berlin", "Oslo", "London", "Paris", "Bruselle", "Kyiv", "Odessa"};
+    map <string, int> fMap1;
+    for (int i = 0; i < sizeof( cities)/sizeof(string); i++)
+    {
+        fMap1[cities[i]] = i + 10;
+    }
     map <string, int> fMap =
     {
         { "New York", 19 },
         { "Mumbai", 21 },
         { "Berlin", 20 }
     }; 
-    thread bgWorker(refreshfMap, fMap);
+    thread bgWorker(refreshfMap, fMap1);
     cin.get();
 }
 
