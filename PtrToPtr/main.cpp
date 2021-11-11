00c17cf65d1177013be2d5a4ce277ae85141ec8e
Ivanov Andrey <ivanovandrey45@gmail.com>
2021-11-11 17:08:43 +0200

    ptr to ptr comments

--- PtrToPtr/main.cpp
+++ PtrToPtr/main.cpp
@@ -6,13 +6,13 @@ int main()
     int a = 7;
     int *ptr = &a;
     int **ptr1 = &ptr;
-    cout << " ptr = " << ptr << endl;
-    cout << " *ptr = " << *ptr << endl;
-    cout << " &ptr = " << &ptr << endl;
-    cout << " ptr1 = " << ptr1 << endl;
-    cout << " *ptr1 = " << *ptr1 << endl;
-    cout << " **ptr1 = " << **ptr1 << endl;
-    cout << " &ptr1 = " << &ptr1 << endl;
+    cout << " ptr = " << ptr << "  Address of variavle." << endl;
+    cout << " *ptr = " << *ptr << "  Value of variavle." << endl;
+    cout << " &ptr = " << &ptr << "  Address of pointer." << endl;
+    cout << " ptr1 = " << ptr1 << "  Address of pointer." << endl;
+    cout << " *ptr1 = " << *ptr1 << " Address of variavle." << endl;
+    cout << " **ptr1 = " << **ptr1<< "  Value of variavle." << endl;
+    cout << " &ptr1 = " << &ptr1 << "  Address of pointer to a pointer." << endl;
     cin.get();
     return 0;
 }
