803eeda63dcc3a41b96cb8f24bab8ebeea1fb7b0
iaa2021 <ivanovandrey45@gmail.com>
2021-12-02 18:52:31 +0200

    Home work 20_17p1035

---
+++ Chapter20/20_17_p1135/20_17.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    //1 way to convert string into char
+    string sentence = "Notice that this string is modified by being broken into smaller strings (tokens).";
+    cout << sentence << endl;
+    char* sent1 = new char [sentence.size() + 1];
+    sentence.copy(sent1, sentence.size() + 1) ;
+    cout << sent1 << endl;
+    cout << "Splitting sent onto the tokens.\n";
+    char *ptr = strtok(sent1, ".,() ");
+    while ( ptr != 0 )
+    {
+        cout << ptr << endl;
+        ptr = strtok( 0, ".,() " );
+    }
+    
+    //2 way to convert string into char
+    string sentence2 = "We know that the strcpy() is a C function. We can avoid using that by using copy instead.";
+    char *sent2 = new char[ sentence2.size() + 1 ];
+    strcpy( sent2, sentence2.c_str() );
+    cout << sent2 << endl;
+    char *ptr2 = strtok( sent2, ".,() " );
+    while (ptr2 != 0)
+    {
+        cout << ptr2 << endl;
+        ptr2 = strtok( 0, ".,() " );
+    }
+    
+    return 0;
+}
---
+++ Chapter20/20_17_p1135/20_17.d
@@ -0,0 +1,2 @@
+obj/20_17.o: \
+ /home/iaa/Desktop/gitStore/MFR2021/Chapter20/20_17_p1135/20_17.cpp
