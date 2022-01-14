23c57f0dca479ee8148561521e9736c6a985f6fd
iaa2021 <ivanovandrey45@gmail.com>
2022-01-14 17:22:43 +0200

    Datel's home work 20_21 14 01 2022

---
+++ Chapter20/20_20min/min.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <iomanip>
+using std::setw;
+#include <ctime>
+using std::time;
+#include <cstdlib>
+using std::rand;
+using std::srand;
+#include <list>
+using std::list;
+#include <iterator>
+using std::iterator;
+using std::ostream_iterator;
+void searchList( int, list<int> );
+int main()
+{
+    srand( time( 0 ) );
+    list<int> intList;
+    for( int i = 0; i < 10; i++ )
+    intList.push_back( rand()%100 + 1 );
+
+    cout << "List in ascending order:\n";
+    copy( intList.begin(), intList.end(), ostream_iterator< int >( cout, ", " ) );
+    cout << "\nList in descending order:\n";
+    copy( intList.rbegin(), intList.rend(), ostream_iterator< int >( cout, ", " ) );
+    cout << endl;
+    int number;
+    cout << "Input number, you'd like to search.\n";
+    cin >> number;
+    searchList( number, intList );
+    return 0;
+}
+void searchList( int number, list<int>intList )
+{
+    
+    int count = 1;
+    for( auto it = intList.begin(); it != intList.end(); it++  )
+    {
+        if( *it == number )
+        cout << "Desired value " << number << " is situated in " << count << " position.\n";
+        else
+        {
+            count++;
+        }
+    }
+
+}
---
+++ Chapter20/20_20min/min.d
@@ -0,0 +1,1 @@
+obj/min.o: /home/iaa/Desktop/gitStore/MFR2021/Chapter20/20_20min/min.cpp
