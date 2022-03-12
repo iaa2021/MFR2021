29d33c7f3dbd27ef88ae07660eee54f37824bd35
iaa2021 <ivanovandrey45@gmail.com>
2022-03-12 11:00:18 +0200

    p1163 Datel 12 03 2022

---
+++ Chapter21/warRepeat/p1163/1163.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <iomanip>
+using std::setw;
+void displayBits( unsigned );
+int main()
+{
+    unsigned iV;
+    cout << "Binary presentation of unsigned value:\n"; 
+    cout << "Input value: ";
+    cin >> iV;
+    displayBits( iV );
+    cout << "\nDemonstration of bitwize operations\n";
+    unsigned iV1;
+    cout << "\nDemonstration of bitwize & operation. Input value1: ";
+    cin >> iV1;
+    displayBits( iV & iV1 );
+    return 0;
+}
+void displayBits( unsigned value )
+{
+    const int Shift = 8 * sizeof( unsigned ) - 1;
+    const unsigned Mask = 1 << Shift;
+    cout << setw( 10 ) << value << " = ";
+    for( unsigned i = 1; i <= Shift + 1; i++ )
+    {
+        cout << ( value & Mask ? '1' : '0' );
+        value <<= 1;
+        if( i % 8 == 0 )
+        cout << ' ';
+    }
+    cout << endl;
+}
---
+++ Chapter21/warRepeat/p1163/1163.d
@@ -0,0 +1,2 @@
+obj/1163.o: \
+ /home/iaa/Desktop/gitStore/MFR2021/Chapter21/warRepeat/p1163/1163.cpp
