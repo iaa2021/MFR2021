619ac851c56ac0bf65e900119223a10d36c15798
iaa2021 <ivanovandrey45@gmail.com>
2022-03-17 13:45:38 +0200

    p1176 17 03 2022

--- Chapter21/warRepeat/p1176/1176.cpp
+++ Chapter21/warRepeat/p1176/1176.cpp
@@ -2,6 +2,10 @@
 using std::cout;
 int main()
 {
-    cout << "Hello world.\n";
+    cout << "According to isdigit " << ( isdigit( '8' )? " 8 is a " : " 8 is not a ") << " digit.\n";
+    cout << "According to isdigit " << ( isdigit( '#' )? " # is a " : " # is not a ") << " digit.\n";
+    cout << "According to isalpha " << ( isalpha( '8' )? " 8 is an " : " 8 is not an ") << " alpha.\n";
+    cout << "According to isalpha " << ( isalpha( 'a' )? " a is an " : " a is not an ") << " alpha.\n";
+    cout << "According to isalpha " << ( isalpha( '&' )? " & is an " : " & is not an ") << " alpha.\n";
     return 0;
 }
