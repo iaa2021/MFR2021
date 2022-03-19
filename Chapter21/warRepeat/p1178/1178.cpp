59cb8359419b143f5415d54903d0b99ab9f1e070
iaa2021 <ivanovandrey45@gmail.com>
2022-03-19 16:50:14 +0200

    p1178 19 03 2022

--- Chapter21/warRepeat/p1178/1178.cpp
+++ Chapter21/warRepeat/p1178/1178.cpp
@@ -8,5 +8,16 @@ using std::toupper;
 int main()
 {
     cout << "According to isupper " << ( isupper( 'D' )?"D is an ": "D is not an " ) << "uppercase letter.\n";
+    cout << "According to isupper " << ( isupper( 'd' )?"d is an ": "d is not an " ) << "uppercase letter.\n";
+    cout << "According to isupper " << ( isupper( '8' )?"8 is an ": "8 is not an " ) << "uppercase letter.\n";
+    cout << "According to isupper " << ( isupper( '$' )?"$ is an ": "$ is not an " ) << "uppercase letter.\n";
+    cout << "According to islower " << ( islower( 'D' )?"D is an ": "D is not an " ) << "lowercase letter.\n\n";
+    cout << "According to islower " << ( islower( 'p' )?"p is a ": "p is not a " ) << "lowercase letter.\n";
+    cout << "According to islower " << ( islower( '5' )?"5 is a ": "5 is not a " ) << "lowercase letter.\n";
+    cout << "According to islower " << ( islower( '!' )?"! is a ": "! is not a " ) << "lowercase letter.\n\n";
+    cout << "Converted to uppercase letter p is " << static_cast<char>( toupper( 'p' ) ) << ".\n";
+    cout << "Converted to uppercase 7 is " << static_cast<char>( toupper( '7' ) ) << ".\n";
+    cout << "Converted to uppercase $ is " << static_cast<char>( toupper( '$' ) ) << ".\n";
+    cout << "Converted to lowercase letter A is " << static_cast<char>( tolower( 'A' ) ) << ".\n";
     return 0;
 }
