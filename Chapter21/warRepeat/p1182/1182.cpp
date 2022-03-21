88a270b047319c5806d8a3afe17c2f77de1845d5
iaa2021 <ivanovandrey45@gmail.com>
2022-03-21 11:20:57 +0200

    p1182 21 03 2022 continue working

--- Chapter21/warRepeat/p1182/1182.cpp
+++ Chapter21/warRepeat/p1182/1182.cpp
@@ -4,7 +4,8 @@ using std::cout;
 using std::atof;
 using std::atoi;
 using std::atol;
-
+using std::strtod;
+using std::strtol;
 int main()
 {
     double d = atof( "99.0" );
@@ -16,5 +17,20 @@ int main()
     long l = atol( "1000000" );
     cout << "The string \" 1000000 \" converted to long is " << l << '\n';
     cout << "The converted value divided by 20 is " << l / 20 << '\n';
+    cout << "Demonstration of strtod function, converting string into double value & residual string.\n";
+    double d1;
+    const char *string1 = "52.38% are admitted.";
+    char *stringPtr;
+    d1 = strtod( string1, &stringPtr );
+    cout << "The string \"" << string1 << "\" is converted  to the double\n value " << d1;
+    cout << " & the string \"" << stringPtr << "\"\n"; 
+    cout << "Demonstration of strtod function, converting string into long value & residual string.\n";
+    long x;
+    const char *string2 = "-1234567abc.";
+    char *stringPtr2;
+    x = strtol( string2, &stringPtr2, 0 );
+    cout << "The string \"" << string1 << "\"\nthe converted value is " << x;
+    cout << "\nThe remainder of the original string is \"" << stringPtr2 << "\"";
+    cout << "\nThe converted value + 567 is " << x + 567 << "\n";
     return 0;
 }
