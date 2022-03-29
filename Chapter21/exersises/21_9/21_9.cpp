d9a3cae1a4571751e730af544e496e65234b96b7
iaa2021 <ivanovandrey45@gmail.com>
2022-03-29 18:25:33 +0300

    exersize 21.9 29 03 2022

---
+++ Chapter21/exersises/21_9/21_9.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+void displayBits( int );
+
+int main()
+{
+    int v;
+    cout << "Input integer value: " << endl;
+    cin >> v;
+    cout << "An value's output in binary view:\n";
+    displayBits( v );
+    cout << "An value's output in binary view after right shift on 4 bits:\n";
+    displayBits( v >> 4 );
+    return 0;
+}
+void displayBits( int value )
+{
+    const int Shift = 8 * sizeof( int ) - 1;
+    const unsigned Mask = 1 << Shift;
+    for( int i = 1; i <= Shift + 1; i++ )
+    {
+        cout << ( value & Mask ? '1' : '0' );
+        value <<= 1;
+        if( i % 8 == 0 )
+        cout << ' ';
+    } 
+    cout << endl;
+}
---
+++ Chapter21/exersises/21_9/21_9.d
@@ -0,0 +1,2 @@
+obj/21_9.o: \
+ /home/iaa/Desktop/gitStore/MFR2021/Chapter21/exersises/21_9/21_9.cpp
