e07da3ddc3082cf6203bcc8f5393d258af959268
iaa2021 <ivanovandrey45@gmail.com>
2022-04-02 17:59:53 +0300

    21.18 02 04 2022

---
+++ Chapter21/exersises/21_18/21_18.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using std ::cout;
+using std ::cin;
+using std ::endl;
+bool multiply( unsigned );
+int main()
+{
+    unsigned number, mask = 1;
+    for ( int i = 0; i < 10; i++, mask <<= 1)
+    {
+        cout << mask << endl;
+    }
+    cout << "\nEnter an integer between 1 and 32000:\n";
+    cin >> number;
+    if ( multiply( number ) )
+    {
+        cout << number << " is multiply of X.\n";
+    }
+    else
+    cout << number << " is not a multiply of X.\n";
+    
+    return 0;
+}
+bool multiply( unsigned value )
+{
+    bool mult = true;
+    for ( int i = 0, mask = 1; i < 10; i++, mask <<= 1)
+    {
+        if ( ( value & mask ) != 0 )
+        {
+            mult = false;
+        }
+    }
+    return mult;
+}
---
+++ Chapter21/exersises/21_18/21_18.d
@@ -0,0 +1,2 @@
+obj/21_18.o: \
+ /home/iaa/Desktop/gitStore/MFR2021/Chapter21/exersises/21_18/21_18.cpp
