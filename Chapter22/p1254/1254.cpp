d256907d965220e11bcd95fe60c5245800df1add
iaa2021 <ivanovandrey45@gmail.com>
2022-04-22 16:57:05 +0300

    p1254 queue 22 04 2022

---
+++ Chapter22/p1254/1254.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::fixed;
+#include <queue>
+using std::queue;
+#include <cstdlib>
+using std::rand;
+using std::srand;
+#include <ctime>
+using std::time;
+#include <iomanip>
+using std::setprecision;
+int main()
+{
+    srand( time( 0 ) );
+    cout << fixed << setprecision( 2 );
+    queue < double > values;
+    cout << "Pushing elements into values.\n";
+    for (size_t i = 0; i < 10; i++)
+    {
+        values.push( static_cast< double >( rand()%100 + 1 ) );
+        cout << values.front() << ' ';
+    }
+    cout << "\nPopping elements from values.\n";
+    while ( !values.empty() )
+    {
+        cout << values.front() << ' ';
+        values.pop();
+    }
+    
+    cout << endl;
+    return 0;
+}
---
+++ Chapter22/p1254/1254.d
@@ -0,0 +1,1 @@
+obj/1254.o: /home/iaa/Desktop/gitStore/MFR2021/Chapter22/p1254/1254.cpp
