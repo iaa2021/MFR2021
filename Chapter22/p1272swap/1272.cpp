8d72982069ba72127374d7dec827003cc302b4e2
Ivanov Andrey <ivanovandrey45@gmail.com>
2022-05-01 16:44:22 +0300

    begin with 1272swap

---
+++ Chapter22/p1272swap/1272.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+#include <iterator>
+using std::ostream_iterator;
+#include <algorithm>
+
+int main()
+{
+    cout << "Hello world.\n";
+    cin.get();
+    return 0;
+}
