0ff21d0217b7001b64263b62dc29a54827fe1dea
Ivanov Andrey <ivanovandrey45@gmail.com>
2022-05-02 11:43:26 +0300

    p1276 inplace_merge

--- Chapter22/p1276inplace_merge/1276.cpp
+++ Chapter22/p1276inplace_merge/1276.cpp
@@ -8,7 +8,9 @@ using std::vector;
 using std::ostream_iterator;
 using std::back_inserter;
 #include <algorithm>
-
+using std::inplace_merge;
+using std::unique_copy;
+using std::reverse_copy;
 int main()
 {
     const int Size = 10;
@@ -17,7 +19,17 @@ int main()
     ostream_iterator<int> output( cout, ", " );
     cout << "Vector v contains: ";
     copy( v.begin(), v.end(), output );
-    
+    inplace_merge( v.begin(), v.begin() + 5, v.end() );
+    cout << "\nAfter implace_merge vector v contains: ";
+    copy( v.begin(), v.end(), output );
+    vector<int> results;
+    unique_copy( v.begin(), v.end(), back_inserter( results ) );
+    cout << "\nAfter unique_copy vector results contains: ";
+    copy( results.begin(), results.end(), output );
+    vector<int> results2;
+    reverse_copy( v.begin(), v.end(), back_inserter( results2 ) );
+    cout << "\nAfter reverse_copy vector results2 contains: ";
+    copy( results2.begin(), results2.end(), output );
     cout << endl;
     return 0;
 }
