409971a4ad35d1e24c2ca9c6897cd713606f2c95
iaa2021 <ivanovandrey45@gmail.com>
2022-05-26 16:22:12 +0300

    Some changes 26 05 2022

---
+++ ApplicationG/p1351argsCommandLine/1351.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::ios;
+using std::cin;
+#include <fstream>
+using std::ofstream;
+using std::ifstream;
+int main( int args, char *argv[] )
+{
+    cout << "Hello world!\n";
+    return 0;
+}
---
+++ ApplicationG/p1351argsCommandLine/1351.d
@@ -0,0 +1,2 @@
+obj/1351.o: \
+ /home/iaa/Desktop/universalGit/ApplicationG/p1351argsCommandLine/1351.cpp
