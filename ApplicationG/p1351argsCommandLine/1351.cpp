8604d409c973a6c3e22379c5ae42b6fc02c6949f
iaa2021 <ivanovandrey45@gmail.com>
2022-05-26 16:38:05 +0300

    p1351 completed

--- ApplicationG/p1351argsCommandLine/1351.cpp
+++ ApplicationG/p1351argsCommandLine/1351.cpp
@@ -8,6 +8,29 @@ using std::ofstream;
 using std::ifstream;
 int main( int args, char *argv[] )
 {
-    cout << "Hello world!\n";
+    if( args != 3 )
+    cout << "Usage: CopyFile infile_name outfile_name\n";
+    else
+    {
+        ifstream inFile( argv[1], ios::in );
+        if( !inFile ) 
+        {
+            cout << argv[1] << " cannot be opened.\n";
+            return -1;   
+        }
+        ofstream outFile( argv[2], ios::out );
+        if( !outFile ) 
+        {
+            cout << argv[2] << " cannot be opened.\n";
+            inFile.close();
+            return -2;   
+        }
+        char c = inFile.get();
+        while ( inFile )
+        {
+            outFile.put( c );
+            c = inFile.get();
+        }
+    }
     return 0;
 }
