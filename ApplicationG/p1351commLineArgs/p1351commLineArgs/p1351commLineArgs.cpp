ec3bc6ab31cea49752e910b7020d21ccac0543c5
Ivanov Andrey <ivanovandrey45@gmail.com>
2022-05-27 14:17:44 +0300

    p1351 command line args in main()

---
+++ ApplicationG/p1351commLineArgs/p1351commLineArgs/p1351commLineArgs.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ios;
+#include <fstream>
+using std::ofstream;
+using std::ifstream;
+int main( int argc, char *argv[] )
+{
+	if (argc != 3)
+		cout << "Usage copyFile inFile_name outFile_name " << endl;
+	else
+	{
+		ifstream inFile(argv[1], ios::in);
+		if ( !inFile )
+		{
+			cout << argv[1] << " cannot be opened.\n";
+			return -1;
+		}
+		ofstream outFile(argv[2], ios::out);
+		if (!outFile)
+		{
+			cout << argv[2] << " cannot be opened.\n";
+			inFile.close();
+			return -2;
+		}
+		char c = inFile.get();
+		while (inFile)
+		{
+			outFile.put(c);
+			c = inFile.get();
+		}
+	}
+	cin.get();
+	return 0;
+}
+
+
