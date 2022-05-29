88129246c44ee1f677537aed99fe27a611371924
Ivanov Andrey <ivanovandrey45@gmail.com>
2022-05-29 16:36:32 +0300

    Making concequence file p992

---
+++ Chapter17/p992concequenceFile/992.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::cerr;
+using std::ios;
+#include <fstream>
+using std::ofstream;
+#include <cstdlib>
+using std::exit;
+int main()
+{
+    ofstream outClientFile( "clients.txt", ios::out );
+    if( !outClientFile )
+    {
+        cerr << "File couldn't be opened.\n";
+        exit( 1 );
+    }
+    cout << "\nEnter the account, name and balance, EOF to end input:\n? ";
+    int acc; char name[ 30 ]; double balance;
+    while ( cin >> acc >> name >> balance )
+    {
+        outClientFile << acc << ' ' << name << ' ' << balance << endl;
+        cout << "? ";
+    }
+    cin.get();
+    return 0;
+}
