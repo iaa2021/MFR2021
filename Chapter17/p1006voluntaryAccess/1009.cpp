18f21a0be2642f51b5204ad657639909f85bb777
Ivanov Andrey <ivanovandrey45@gmail.com>
2022-05-30 12:43:44 +0300

    p1009voluntaryAccess and in VS too

---
+++ Chapter17/p1006voluntaryAccess/1009.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using std::cerr;
+using std::endl;
+using std::ios;
+using std::cin;
+#include <fstream>
+using std::ofstream;
+#include <cstdlib>
+using std::exit;
+#include "ClientData.h"
+int main()
+{
+    ofstream outCredit( "credit.txt", ios::binary );
+    if( !outCredit )
+    {
+        cerr << "File cannot be opened.\n";
+        exit( 1 );
+    }
+    ClientData blankClient;
+    for ( int i = 0; i < 100; i++ )
+    {
+        outCredit.write( reinterpret_cast< const char * >( &blankClient ), sizeof( ClientData ) );
+    }
+    cin.get();
+    return 0;
+}
