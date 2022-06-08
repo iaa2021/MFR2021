7cae9194c61e286c0b1ca2647bac9ea73288520c
Ivanov Andrey <ivanovandrey45@gmail.com>
2022-06-08 11:45:14 +0300

    read from voluntary access file p1013

---
+++ Chapter17/p1013ReadingFromVolAccFile/p1013ReadingFromVolAccFile/ClientData.cpp
@@ -0,0 +1,50 @@
+#include <string>
+using std::string;
+#include "ClientData.h"
+ClientData::ClientData(int accValue, string lastNameValue, string firstNameValue, double balanceValue)
+{
+    setAccNumber(accValue);
+    setLastName(lastNameValue);
+    setFirstName(firstNameValue);
+    setBalance(balanceValue);
+}
+int ClientData::getAccNumber() const
+{
+    return acc;
+}
+void ClientData::setAccNumber(int accValue)
+{
+    acc = accValue;
+}
+string ClientData::getLastName() const
+{
+    return lastName;
+}
+void ClientData::setLastName(string lnString)
+{
+    const char* lnValue = lnString.data();
+    int length = lnString.size();
+    length = (length < 15 ? length : 14);
+    strncpy_s(lastName, lnValue, length);
+    lastName[length] = '\0';
+}
+string ClientData::getFirstName() const
+{
+    return firstName;
+}
+void ClientData::setFirstName(string fnString)
+{
+    const char* fnValue = fnString.data();
+    int length = fnString.size();
+    length = (length < 10 ? length : 9);
+    strncpy_s(firstName, fnValue, length);
+    firstName[length] = '\0';
+}
+double ClientData::getBalance() const
+{
+    return balance;
+}
+void ClientData::setBalance(double balanceValue)
+{
+    balance = balanceValue;
+}
---
+++ Chapter17/p1013ReadingFromVolAccFile/p1013ReadingFromVolAccFile/p1013ReadingFromVolAccFile.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::showpoint;
+using std::left;
+using std::right;
+using std::ios;
+using std::fixed;
+using std::endl;
+#include <iomanip>
+using std::setw;
+using std::setprecision;
+#include <fstream>
+using std::ifstream;
+using std::ostream;
+#include <cstdlib>
+using std::exit;
+#include "ClientData.h"
+void outputLine(ostream&, const ClientData&);
+int main()
+{
+    ifstream inCredit("credit.txt", ios::in);
+    if (!inCredit)
+    {
+        cerr << "File cannot be opened.\n";
+        exit(1);
+    }
+    cout << left << setw(10) << "Account" << setw(16) << "Last name" << setw(11) << "First name";
+    cout << setw(10) << right << "Balance" << endl;
+    ClientData client;
+    inCredit.read(reinterpret_cast<char*>(&client), sizeof(ClientData));
+    while ( inCredit && !inCredit.eof() )
+    {
+        if (client.getAccNumber() != 0)
+            outputLine(cout, client);
+ 
+        inCredit.read(reinterpret_cast<char*>(&client), sizeof(ClientData));
+    }
+    cin.get();
+    return 0;
+}
+void outputLine(ostream& output, const ClientData& record)
+{
+    output << left << setw(10) << record.getAccNumber() << setw(16) << record.getLastName() << setw(11) << record.getFirstName()
+    << setw(10) << setprecision(2) << right << fixed << showpoint <<  record.getBalance() << endl;
+}
+
