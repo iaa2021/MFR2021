bdb217b723ffdbdf32e0febc84dd4e8ee360cc23
Ivanov Andrey <ivanovandrey45@gmail.com>
2022-06-09 11:39:43 +0300

    p1015 transactions handling programs

--- Chapter17/p1015transactionsHandlingProgram/p1015transactionsHandlingProgram/p1015transactionsHandlingProgram.cpp
+++ Chapter17/p1015transactionsHandlingProgram/p1015transactionsHandlingProgram/p1015transactionsHandlingProgram.cpp
@@ -28,7 +28,36 @@ int getAccount(const char*const);
 enum Choices { PRINT = 1, UPDATE, NEW, DELETE, END };
 int main()
 {
-    std::cout << "Hello World!\n";
+    fstream inOutCredit("credit.txt", ios::in | ios::app);
+    if (!inOutCredit)
+    {
+        cerr << "File cannot be opened.\n";
+        exit(1);
+    }
+    int choice;
+    cout << "Input your choice" << endl;
+    while ( (choice = enterChoice()) != END  )
+    {
+        switch (choice)
+        {
+        case PRINT:
+            createTextFile(inOutCredit);
+            break;
+        case UPDATE:
+            updateRecord(inOutCredit);
+            break;
+        case NEW:
+            newRecord(inOutCredit);
+            break;
+       
+        
+        default:
+            cerr << "You've entered wrong choice.\n";
+            break;
+        }
+        inOutCredit.clear();
+    }
+    return 0;
 }
 int enterChoice()
 {
@@ -83,4 +112,35 @@ void updateRecord(fstream& updateFile)
     }
     else
         cerr << "Account #" << accNmb << " has no information.\n";
+}
+void newRecord(fstream& insertInFile)
+{
+    int accNmb = getAccount("Enter account to insert:\n");
+    ClientData client;
+    insertInFile.seekg((accNmb - 1) * sizeof(ClientData));
+    insertInFile.read(reinterpret_cast<char*>(&client), sizeof(ClientData));
+    if (client.getAccNumber() != 0)
+        cout << "Account #" << accNmb << " already contains information.\n";
+    else
+    {
+        char lName[15]; char fName[10]; double balance;
+        cout << "Input last name, first name, balance:\n";
+        cin >> setw(15) >> lName; cin >> setw(10) >> fName; cin >> balance;
+        client.setAccNumber(accNmb);
+        client.setLastName(lName);
+        client.setFirstName(fName);
+        client.setBalance(balance);
+        insertInFile.seekp((accNmb - 1) * sizeof(ClientData));
+        insertInFile.write(reinterpret_cast<const char*>(&client), sizeof(ClientData));
+    }
+}
+int getAccount(const char* const prompt)
+{
+    int accNmb;
+    do
+    {
+        cout << prompt << " 1 - 100, 0 to end input\n";
+        cin >> accNmb;
+    } while (accNmb > 0 && accNmb <= 100);
+    return accNmb;
 }
