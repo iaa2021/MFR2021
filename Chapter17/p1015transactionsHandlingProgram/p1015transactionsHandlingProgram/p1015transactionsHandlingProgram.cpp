63f92539ae91dda68d62c5899e20675a28b17740
Ivanov Andrey <ivanovandrey45@gmail.com>
2022-06-09 18:27:06 +0300

    some changes in p1015

--- Chapter17/p1015transactionsHandlingProgram/p1015transactionsHandlingProgram/p1015transactionsHandlingProgram.cpp
+++ Chapter17/p1015transactionsHandlingProgram/p1015transactionsHandlingProgram/p1015transactionsHandlingProgram.cpp
@@ -19,8 +19,7 @@ using std::setprecision;
 using std::exit;
 #include "ClientData.h"
 int enterChoice();
-void createTextFile(fstream&);
-void updateRecord(fstream&);
+
 void newRecord(fstream&);
 void deleteRecord(fstream&);
 void outputLine(ostream&, const ClientData&);
@@ -40,12 +39,7 @@ int main()
     {
         switch (choice)
         {
-        case PRINT:
-            createTextFile(inOutCredit);
-            break;
-        case UPDATE:
-            updateRecord(inOutCredit);
-            break;
+        
         case NEW:
             newRecord(inOutCredit);
             break;
@@ -68,51 +62,8 @@ int enterChoice()
     cin >> menuChoice;
     return menuChoice;
 }
-void createTextFile(fstream& readFromFile)
-{
-    //create text file
-    ofstream outPrintFile("print.txt", ios::out);
-    if (!outPrintFile)
-    {
-        cerr << "File cannot be opened.\n";
-        exit(1);
-    }
-    outPrintFile << left << setw(10) << "Account" << setw(16) << "Last Name" << setw(11) << "First Name" << right << setw(10) << "Balance" << endl;
-    readFromFile.seekg(0);
-    ClientData client;
-    readFromFile.read(reinterpret_cast<char*>(&client), sizeof(ClientData));
-    while (!readFromFile.eof())
-    {
-        if (client.getAccNumber() != 0)
-            outputLine(outPrintFile, client);
 
-        readFromFile.read(reinterpret_cast<char*>(&client), sizeof(ClientData));
-    }
-}
-void updateRecord(fstream& updateFile)
-{
-    //to get account number
-    int accNmb = getAccount("Enter account to update:\n");
-    updateFile.seekg((accNmb - 1) * sizeof(ClientData) );
-    ClientData client;
-    updateFile.read(reinterpret_cast<char*>(&client), sizeof(ClientData));
-    //to update record
-    if (client.getAccNumber() != 0)
-    {
-        outputLine(cout, client); //to output record
-        //to ask user about kind of transaction
-        cout << "Enter charge(+) or payment(-): ";
-        double transaction;
-        cin >> transaction;
-        double oldBalance = client.getBalance();
-        client.setBalance(oldBalance + transaction);
-        outputLine(cout, client); //to output updated record
-        updateFile.seekp((accNmb - 1) * sizeof(ClientData));
-        updateFile.write(reinterpret_cast<const char*>(&client), sizeof(ClientData));
-    }
-    else
-        cerr << "Account #" << accNmb << " has no information.\n";
-}
+
 void newRecord(fstream& insertInFile)
 {
     int accNmb = getAccount("Enter account to insert:\n");
