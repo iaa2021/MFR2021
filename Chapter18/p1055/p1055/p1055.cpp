1014352a3007ab4d904ee3730815dfbe2799d03e
Ivanov Andrey <ivanovandrey45@gmail.com>
2022-07-06 18:22:23 +0300

    p1055 istringstream

---
+++ Chapter18/p1055/p1055/p1055.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ostream;
+#include <string>
+using std::string;
+#include <sstream>
+using std::istringstream;
+using std::ostringstream;
+#include <vector>
+using std::vector;
+#include <algorithm>
+using std::copy;
+#include <iterator>
+using std::ostream_iterator;
+int main()
+{
+    string line("Input such 784 lines. Their weight is 17.45 tonnes, height 147 metres, time 24.32 hours.\n");
+    istringstream inpLine(line);
+    vector<double>dVect; vector<string>sVect; vector<int>iVect;
+    ostream_iterator<string> dIt(cout, " ");
+    string s; double d; int i;
+    ostringstream outpLine(line);
+    cout << outpLine.str() << endl;
+    while (inpLine >> s)
+    {
+        sVect.push_back(s);
+    }
+    copy(sVect.begin(), sVect.end(), dIt);
+    cin.get();
+    return 0;
+}
+
