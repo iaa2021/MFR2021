2f6f0e0bb9137cb7478c1d048affb39dcee5c7a7
iaa2021 <ivanovandrey45@gmail.com>
2022-07-23 18:54:21 +0300

    ex18.3

---
+++ Chapter18/ex18_13/ex18_13/ex18_13.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+int main()
+{
+    string text;
+    cout << "Input text:\n";
+    getline(cin, text);
+    for (size_t i = 0; i < text.size(); i++)
+    {
+        if ( text.at(i) >= 'A' && text.at(i) <= 'Z')
+        {
+            text.at(i) += 'a' - 'A';
+        }
+        else
+        {
+            text.at(i) -= 'a' - 'A';
+        }
+    }
+    cout << "Reverse direction is:\n";
+    for (int i = text.size() - 1; i >= 0; i--)
+    {
+        cout << text.at(i);
+    }
+    cin.get();
+    return 0;
+}
+
