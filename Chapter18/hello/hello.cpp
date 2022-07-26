019ae806e175805284c59af97d575caaa473a7a2
iaa2021 <ivanovandrey45@gmail.com>
2022-07-26 09:41:53 +0300

    Installing visual studio code a testing it

---
+++ Chapter18/hello/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+int main()
+{
+    cout << "Hello world.\n";
+    int a = 154;
+    int b;
+    cout << "Input b: ";
+    cin >> b;
+    cout << ( a + b ) << endl;
+    return 0;
+}
---
+++ Chapter18/hello/hello.d
@@ -0,0 +1,2 @@
+obj/hello.o: \
+ C:\Users\ivano\Desktop\GitStore\MFR2021\Chapter18\hello/hello.cpp
