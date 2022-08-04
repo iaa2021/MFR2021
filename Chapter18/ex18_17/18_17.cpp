1be340ba505ce5747f48f074d539a68f18c8707f
iaa2021 <ivanovandrey45@gmail.com>
2022-08-04 11:05:49 +0300

    working at ex18.17

--- Chapter18/ex18_17/18_17.cpp
+++ Chapter18/ex18_17/18_17.cpp
@@ -15,9 +15,11 @@ using std::ostream_iterator;
 #include <cctype>
 using std::tolower;
 using std::isupper;
+#include <queue>
+using std::deque;
 int main()
 {
-    string text; char c; vector <string> v;
+    string text; char c; vector <string> v, p; int l = 0; deque <char> word;
     ostream_iterator <string> it(cout, ", ");
     cout << "Input text:\n";
     getline( cin, text );
@@ -43,5 +45,26 @@ int main()
     cout << "\nVector consist of:\n";
     copy(v.begin(), v.end(), it);
     cout << endl;
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        for( size_t j = 0; j < v[i].size(); j++ )
+        word.push_front(v[i].at(j));
+        for (size_t k = 0; k < v[i].size(); k++)
+        {
+            if (word.at(k) != v.at(i).at(k))
+                l++;
+        }
+        if (l == 0)
+        p.push_back(v.at(i));
+    }
+    if (p.empty())
+    cout << "\nThere are no palindroms in this text.\n";
+    else
+    {
+        cout << "\nThere are such palindroms in this text.\n";
+        copy(p.begin(), p.end(), it);
+    }
+    cout << endl;
+    delete[] ptr;
     return 0;
 }
