eb47209cc4f924b4f7189c7cfdb880c917fd2997
iaa2021 <ivanovandrey45@gmail.com>
2022-08-08 11:02:14 +0300

    ex18.20 08.08.2022

--- Chapter18/ex18_19/ex18_19/ex18_19.cpp
+++ Chapter18/ex18_19/ex18_19/ex18_19.cpp
@@ -7,11 +7,21 @@ using std::string;
 int main()
 {
     string text, stars("*****");
-    std::cout << "Input text:\n";
+    cout << "Input text:\n";
     getline(cin, text);
     cout << "\nNew text is:\n";
     text.insert(text.size() / 2, stars);
     cout << text;
+    cout << "\nCompliting exersize 18.19.\n";
+    string text1;
+    cout << "Input text1:\n";
+    getline(cin, text1);
+    for (size_t i = 0; i < text1.size(); i++)
+    {
+        if ((text1.at(i) == 'b' && text1.at(i + 1) == 'y') || (text1.at(i) == 'B' && text1.at(i + 1) == 'Y'))
+            text1.erase(i, 2);
+    }
+    cout << "\nNew text is:\n" << text1 << endl;
     cin.get();
     return 0;
 }
