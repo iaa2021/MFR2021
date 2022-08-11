9ec4958fe375372395a9c1ebf88b1a687191be64
iaa2021 <ivanovandrey45@gmail.com>
2022-08-11 10:25:40 +0300

    palindrom

---
+++ Chapter18/Palindrom/pal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+#include <cstring>
+using std::strtok;
+#include <iterator>
+using std::back_inserter;
+using std::ostream_iterator;
+int main()
+{
+    
+    cout << "hello" << endl;
+    return 0;
+
+}
