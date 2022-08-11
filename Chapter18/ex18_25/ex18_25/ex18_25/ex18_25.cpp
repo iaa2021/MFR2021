94dd53608d22cebec974ae285f7ce20062abcf6f
iaa2021 <ivanovandrey45@gmail.com>
2022-08-11 16:00:16 +0300

    ex18.25 completed

--- Chapter18/ex18_25/ex18_25/ex18_25/ex18_25.cpp
+++ Chapter18/ex18_25/ex18_25/ex18_25/ex18_25.cpp
@@ -9,13 +9,19 @@ using std::string;
 using std::setw;
 int main()
 {
-    string alphabet("abcdefghijklmnopqrstuvwxyz}");
+    string alphabet("abcdefghijklmnopqrstuvwxyz}"), word, word1;
 	for (size_t i = 0; i <= alphabet.size()/2; i++)
 	{
 		for (size_t j = i; j <= i * 2; j++)
+			word += alphabet.at(j);
+		if (i > 0)
 		{
-			cout << setw((alphabet.size() / 2) - j) << right << alphabet.at(j);
+			for (size_t k = (i * 2) - 1; k >= i; k--)
+				word1 += alphabet.at(k);
 		}
+		cout << right << setw((alphabet.size() / 2) + 1) << word;
+		cout << left << setw((alphabet.size() / 2) + 1) << word1;
+		word.clear(); word1.clear();
 		cout << endl;
 	}
 	return 0;
