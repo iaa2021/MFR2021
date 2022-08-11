9ae47a177a564002ab4673c160e02c217585e005
iaa2021 <ivanovandrey45@gmail.com>
2022-08-11 12:34:08 +0300

    completing palindrom in Visual Studio

---
+++ Chapter18/Palindrom/Palindrom/Palindrom/Palindrom.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string word; int l = 0;
+    cout << "Input word\n";
+    getline(cin, word);
+    for (int i = 0; i < word.size(); i++)
+    {
+        if (word[i] != word[word.size() - 1 - i])
+        l++;
+    }
+    cout << "The word " << word << " size is " << word.size() << endl;
+    if (l == 0)
+        cout << "The word " << word << " is palindrom.\n";
+    else
+        cout << "The word " << word << " is not palindrom.\n";
+    return 0;
+}
