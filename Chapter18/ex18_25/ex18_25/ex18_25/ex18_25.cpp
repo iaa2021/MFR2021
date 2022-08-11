f5b82ff8b1846302439fc52063d4083e7c2521c9
iaa2021 <ivanovandrey45@gmail.com>
2022-08-11 14:13:19 +0300

    ex18_25

---
+++ Chapter18/ex18_25/ex18_25/ex18_25/ex18_25.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+using std::left;
+using std::right;
+#include <string>
+using std::string;
+#include <iomanip>
+using std::setw;
+int main()
+{
+    string alphabet("abcdefghijklmnopqrstuvwxyz}");
+	for (size_t i = 0; i <= alphabet.size()/2; i++)
+	{
+		for (size_t j = i; j <= i * 2; j++)
+		{
+			cout << setw((alphabet.size() / 2) - j) << right << alphabet.at(j);
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
