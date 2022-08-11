997e5aadb1ae931a16c079b14796228461abf1cc
iaa2021 <ivanovandrey45@gmail.com>
2022-08-11 11:05:23 +0300

    palindrom in VS

---
+++ Chapter18/Palindrom/Palindrom/Palindrom.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+#include <cstring>
+using std::strtok;
+#include <iterator>
+using std::ostream_iterator;
+using std::back_inserter;
+#include <algorithm>
+using std::copy;
+#include <vector>
+using std::vector;
+int main()
+{
+    string text; int l; 
+    cout << "Input text:\n";
+    getline(cin, text);
+    char* ptr = new char[text.size()];
+    for (size_t i = 0; i < text.size(); i++)
+    {
+        ptr[i] = text.at(i);
+    }
+    cout << "array is:\n";
+    for (size_t i = 0; i < text.size(); i++)
+    {
+        cout << ptr[i];
+    }
+    cout << endl;
+    return 0;
+}
+
