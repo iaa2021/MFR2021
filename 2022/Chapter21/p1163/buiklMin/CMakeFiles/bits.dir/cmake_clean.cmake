8f85f13c09c12ed7ea3b6340440866befd713df9
iaa2021 <ivanovandrey45@gmail.com>
2022-08-18 20:10:49 +0300

    adding MinGW makefiles in p1163

---
+++ 2022/Chapter21/p1163/buiklMin/CMakeFiles/bits.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bits.dir/1163.cpp.obj"
+  "CMakeFiles/bits.dir/1163.cpp.obj.d"
+  "bits.exe"
+  "bits.exe.manifest"
+  "bits.pdb"
+  "libbits.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bits.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
