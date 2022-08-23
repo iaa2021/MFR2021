a72ad6647779c821a374e02ff50da69cc2c297c6
iaa2021 <ivanovandrey45@gmail.com>
2022-08-23 14:29:33 +0300

    adding cmake in p1182

---
+++ Chapter21/p1182/build/CMakeFiles/1182.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "1182"
+  "1182.pdb"
+  "CMakeFiles/1182.dir/1182.cpp.o"
+  "CMakeFiles/1182.dir/1182.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1182.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
