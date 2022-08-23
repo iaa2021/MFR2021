aa4996dcb2a699310ba7e0f65e696fafcfaaff22
iaa2021 <ivanovandrey45@gmail.com>
2022-08-23 12:09:09 +0300

    p1169 23 08 2022

---
+++ Chapter21/p1169/1169.d
@@ -0,0 +1,1 @@
+obj/1169.o: /home/iaa/Desktop/gitStore/MFR2021/Chapter21/p1169/1169.cpp
---
+++ Chapter21/p1169/buildN/CMakeFiles/p1169.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p1169.dir/1169.cpp.o"
+  "CMakeFiles/p1169.dir/1169.cpp.o.d"
+  "p1169"
+  "p1169.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p1169.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
