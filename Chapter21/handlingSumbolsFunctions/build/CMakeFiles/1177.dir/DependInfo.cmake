57df5e0731e300694103342cfeb460ccec111224
iaa2021 <ivanovandrey45@gmail.com>
2022-08-23 14:21:50 +0300

    adding cmake in handlingSumbolsFunctions

---
+++ Chapter21/handlingSumbolsFunctions/build/CMakeFiles/1177.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/iaa/Desktop/gitStore/MFR2021/Chapter21/handlingSumbolsFunctions/1177.cpp" "CMakeFiles/1177.dir/1177.cpp.o" "gcc" "CMakeFiles/1177.dir/1177.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Chapter21/handlingSumbolsFunctions/build/CMakeFiles/1177.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "1177"
+  "1177.pdb"
+  "CMakeFiles/1177.dir/1177.cpp.o"
+  "CMakeFiles/1177.dir/1177.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1177.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
