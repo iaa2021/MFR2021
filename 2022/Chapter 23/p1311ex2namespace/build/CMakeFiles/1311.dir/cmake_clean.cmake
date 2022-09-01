4755554d25d4484ccbac624453a8d6223f4a21fd
iaa2021 <ivanovandrey45@gmail.com>
2022-09-01 12:05:27 +0300

    ex2 of p1311

---
+++ 2022/Chapter 23/p1311ex2namespace/build/CMakeFiles/1311.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "1311.exe"
+  "1311.exe.manifest"
+  "1311.pdb"
+  "CMakeFiles/1311.dir/1311.cpp.obj"
+  "CMakeFiles/1311.dir/1311.cpp.obj.d"
+  "lib1311.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1311.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
