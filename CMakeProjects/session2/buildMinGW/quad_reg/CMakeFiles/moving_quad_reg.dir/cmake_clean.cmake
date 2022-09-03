2256871a0d3787f2e2f2e015d55f9e83cb063ee6
iaa2021 <ivanovandrey45@gmail.com>
2022-09-03 17:17:33 +0300

    creating static lib in session2 monomials

---
+++ CMakeProjects/session2/buildMinGW/quad_reg/CMakeFiles/moving_quad_reg.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/moving_quad_reg.dir/MovingQuadReg.cpp.obj"
+  "CMakeFiles/moving_quad_reg.dir/MovingQuadReg.cpp.obj.d"
+  "libmoving_quad_reg.a"
+  "libmoving_quad_reg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/moving_quad_reg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
