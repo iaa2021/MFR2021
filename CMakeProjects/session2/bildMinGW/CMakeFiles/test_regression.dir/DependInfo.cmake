80fc82fbf5c6c933d8a7c3a676491f44a4593cf8
iaa2021 <ivanovandrey45@gmail.com>
2022-09-03 16:20:08 +0300

    session2 CMake monomials

---
+++ CMakeProjects/session2/bildMinGW/CMakeFiles/test_regression.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ivano/Desktop/GitStore/MFR2021/CMakeProjects/session2/quad_reg/MovingQuadReg.cpp" "CMakeFiles/test_regression.dir/quad_reg/MovingQuadReg.cpp.obj" "gcc" "CMakeFiles/test_regression.dir/quad_reg/MovingQuadReg.cpp.obj.d"
+  "C:/Users/ivano/Desktop/GitStore/MFR2021/CMakeProjects/session2/test_regression.cpp" "CMakeFiles/test_regression.dir/test_regression.cpp.obj" "gcc" "CMakeFiles/test_regression.dir/test_regression.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
