dcec973f6e8786202a720886e58ecd8bd4bb0e70
iaa2021 <ivanovandrey45@gmail.com>
2022-09-05 13:54:57 +0300

    p1311namespace from main adapted for ubi

---
+++ 2022/Chapter 23/p1311namespace/build/CMakeFiles/1311.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "1311"
+  "1311.pdb"
+  "CMakeFiles/1311.dir/1311.cpp.o"
+  "CMakeFiles/1311.dir/1311.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1311.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
