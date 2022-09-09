212b586d8ae78e49ed732f280901ae26d6aa4b7c
iaa2021 <ivanovandrey45@gmail.com>
2022-09-09 12:25:48 +0300

    ex22.13 and 22.14 in 22.12

--- CMakeProjects/Chapter22/ex22_12/22_12.cpp
+++ CMakeProjects/Chapter22/ex22_12/22_12.cpp
@@ -13,13 +13,67 @@ int main()
     int value; ostream_iterator<int> it( cout, ", " );
     cout << "Input number: ";
     cin >> value;
-    cout << "\nNumber has such odd deleters:\n";
-    vector <int> deleters;
+    vector <int> deleters, odds;
     for( int i = 2; i <= value/2; i++ )
     {
         if ( value % i == 0 )
         deleters.push_back(i);
     }
-    copy( deleters.begin(), deleters.end(), it );
+    if( deleters.empty() )
+    {
+    cout << "\nNumber " << value << " is odd.\n";
+        exit( 1 );
+    }
+    else
+    {
+        cout << "\nNumber has such deleters:\n";
+        copy( deleters.begin(), deleters.end(), it );
+    }
+    cout << "\nNumber has such odd deleters:\n";
+    for (size_t i = 0; i < deleters.size(); i++)
+    {
+        for (int j = 2; j <= deleters.at( i )/ 2; j++)
+        {
+            if( deleters.at( i ) % j == 0 )
+            deleters.at( i ) = 0;
+        } 
+        if( deleters.at( i ) != 0 )
+        odds.push_back( deleters.at( i ) );
+    }
+    copy( odds.begin(), odds.end(), it );
+    deleters.clear();
+    cout << "\nNumber can be composed from such deleters:\n";
+    int total = value;
+    while ( total != 1 )
+    {
+       for (size_t i = 0; i < odds.size(); i++)
+       {
+        if( total % odds.at( i ) == 0 )
+            {
+                total /= odds.at( i );
+                deleters.push_back( odds.at( i ) );
+            }
+       }
+        while( total != 1 )
+        {
+            for (size_t j = 0; j < odds.size(); j++)
+            {
+                if( total % odds.at( j ) == 0 )
+                {
+                    total /= odds.at( j );
+                    deleters.push_back( odds.at( j ) );
+                }
+            }
+        }
+    }
+    cout << "Number = ";
+    for (size_t i = 0; i < deleters.size(); i++)
+    {
+        if( i < deleters.size() - 1 )
+        cout << deleters.at( i ) << " * ";
+        if( i == deleters.size() - 1 )
+        cout << deleters.at( i ) << endl; 
+    }
+    
     return 0;
 }
