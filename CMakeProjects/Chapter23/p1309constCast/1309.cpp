f31a31f45e01f060355fc8c90a7f333cca41546c
iaa2021 <ivanovandrey45@gmail.com>
2022-09-09 17:53:31 +0300

    p1309 complete

--- CMakeProjects/Chapter23/p1309constCast/1309.cpp
+++ CMakeProjects/Chapter23/p1309constCast/1309.cpp
@@ -10,10 +10,16 @@ using std::toupper;
 const char *maximum( const char *, const char * );
 int main()
 {
-    char s1[] = "Hello";
-    char s2[] = "Good bye";
+    char s1[] = "I'm new to linux environment";
+    char s2[] = "When I try to compile my code";
     char *max = const_cast<char *>( maximum( s1, s2 ) );
     cout << "Maximum of these two arrays " << s1 << " and " << s2 << "\nis " << max << endl;
+    cout << "Capitalisated one is:\n";
+    for (size_t i = 0; i < strlen(max); i++)
+    {
+        max[ i ] = toupper( max[ i ] );
+    }
+    cout << max << endl;
     cin.get();
     return 0;
 }
