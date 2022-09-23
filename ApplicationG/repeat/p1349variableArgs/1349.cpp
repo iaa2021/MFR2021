16de2a795ddffc6c123b1a691b9b0b4c5e7b814d
iaa2021 <ivanovandrey45@gmail.com>
2022-09-23 11:35:02 +0300

    p1349 variable arguments in function

---
+++ ApplicationG/repeat/p1349variableArgs/1349.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::ios;
+#include <iomanip>
+using std::setw;
+using std::setiosflags;
+using std::setprecision;
+using std::fixed;
+#include <cstdarg>
+using std::va_list;
+double average( int, ... );
+int main()
+{
+    double d1 = 3.25;
+    double d2 = 4.73;
+    double d3 = 7.28;
+    double d4 = 8.21;
+    cout << fixed << setprecision( 2 );
+    cout << "double1 = " << d1 << "\ndouble2 = " << d2;
+    cout  << "\ndouble3 = " << d3 << "\ndouble4 = " << d4 << endl;
+    cout << "The sum of double1 and double2 = " << average( 2, d1, d2 ) << endl;
+    cout << "The sum of double1 and double2 and double3 = " << average( 3, d1, d2, d3 ) << endl;
+    cout << "The sum of double1 and double2 and double3 and double4 = " << average( 4, d1, d2, d3, d4 ) << endl;
+    return 0;
+}
+double average( int count, ... )
+{
+    double total = 0;
+    va_list list;//keeping info for va_start
+    va_start( list, count );
+    for (int i = 0; i < count; i++)
+        total += va_arg( list, double );
+    
+    va_end( list );
+    return total;
+}
---
+++ ApplicationG/repeat/p1349variableArgs/1349.d
@@ -0,0 +1,2 @@
+obj/1349.o: \
+ /home/iaa/Desktop/gitStore/MFR2021/ApplicationG/repeat/p1349variableArgs/1349.cpp
