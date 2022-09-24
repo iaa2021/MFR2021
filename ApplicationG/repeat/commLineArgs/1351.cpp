72d1540ebb544a232b7dfbde67be4fa769332a02
iaa2021 <ivanovandrey45@gmail.com>
2022-09-24 11:15:44 +0300

    command line arguments

---
+++ ApplicationG/repeat/commLineArgs/1351.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::ios;
+#include <fstream>
+using std::ifstream;
+using std::ofstream;
+
+int main( int argc, char *argv[] )
+{
+    if( argc != 3 )
+    cout << "Usage copyFile inFile_name outFile_name.\n";
+    else
+    {
+        ifstream inFile( argv[ 1 ], ios::in );
+        if ( !inFile )
+        {
+            cout << argv[ 1 ] << " cannot be opened.\n";
+            return -1;
+        }
+        ofstream outFile( argv[ 2 ], ios::out );
+        if ( !outFile )
+        {
+            cout << argv[ 2 ] << " cannot be opened.\n";
+            inFile.close();
+            return -2;
+        }
+        char c = inFile.get();//to read first character
+        while ( inFile )
+        {
+            outFile.put( c );
+            c = inFile.get();
+        }
+    }
+    return 0;
+}
---
+++ ApplicationG/repeat/commLineArgs/1351.d
@@ -0,0 +1,2 @@
+obj/1351.o: \
+ /home/iaa/Desktop/gitStore/MFR2021/ApplicationG/repeat/commLineArgs/1351.cpp
