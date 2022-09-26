461c4ce80a7dcad1491d76923ac48656ae45d3a5
iaa2021 <ivanovandrey45@gmail.com>
2022-09-26 19:18:19 +0300

    p1364unions

--- ApplicationG/1364unions/1364.d
+++ ApplicationG/1364unions/1364.d
@@ -1,2 +1,3 @@
 obj/1364.o: \
- /home/iaa/Desktop/gitStore/MFR2021/ApplicationG/1364unions/1364.cpp
+ /home/iaa/Desktop/gitStore/MFR2021/ApplicationG/1364unions/1364.cpp \
+ /home/iaa/Desktop/gitStore/MFR2021/ApplicationG/1364unions/config.h
