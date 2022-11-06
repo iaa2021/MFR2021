fa06278872e00f2e7a20d3817fbcf077b5a04674
iaa2021 <ivanovandrey45@gmail.com>
2022-11-06 20:35:05 +0200

    ex6.34 - 6.38 6.11.2022

---
+++ CMakeProjects/Chapter6/ex6_34/6_34.d
@@ -0,0 +1,2 @@
+obj/6_34.o: \
+ C:\Users\ivano\Desktop\gitStore\MFR2021\CMakeProjects\Chapter6\ex6_34/6_34.cpp
---
+++ CMakeProjects/Chapter6/ex6_34/buildMinGW/CMakeFiles/6_34.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "6_34.exe"
+  "6_34.exe.manifest"
+  "6_34.pdb"
+  "CMakeFiles/6_34.dir/6_34.cpp.obj"
+  "CMakeFiles/6_34.dir/6_34.cpp.obj.d"
+  "lib6_34.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6_34.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
