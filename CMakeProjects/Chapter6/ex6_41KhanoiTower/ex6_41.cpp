418912e9015e616f72e279ed45f64244ddd625f7
iaa2021 <ivanovandrey45@gmail.com>
2022-11-22 18:45:06 +0200

    khanoi in work 22.11.2022

--- CMakeProjects/Chapter6/ex6_41KhanoiTower/ex6_41.cpp
+++ CMakeProjects/Chapter6/ex6_41KhanoiTower/ex6_41.cpp
@@ -14,14 +14,14 @@ struct peg
     string name;
     int number;
 };
-void khanoi( int, peg, peg, peg );
+void khanoi( int, int, peg, peg, peg );
 
 int main()
 {
     peg a, b, c;
     a.name = "a"; b.name = "b"; c.name = "c";
     a.number = 0; b.number = 0; c.number = 0;
-    int number;
+    int number, odd;
     cout << "Input number: ";
     cin >> number;
     if( number > 0 )
@@ -32,29 +32,67 @@ int main()
         }
         a.number = number;
     }
-    khanoi( number, a, b, c );
+    if( number % 2 == 0 )
+    odd = 1;
+    else 
+    odd = 0;
+    khanoi( number, odd, a, b, c );
     return 0;
 }
-void khanoi( int number, peg a, peg b, peg c )
+void khanoi( int number, int odd, peg a, peg b, peg c )
 {
-    number = a.number;
-    if( number == 1 )
+    while( a.number == 0 && b.number == 0 && c.number == number )
     {
-        b.st.push( a.st.top() );
-        a.st.pop();
-        a.number--; b.number++;
-        cout << a.name << " -> " << b.name << endl;
-    }
-    while( number > 1 )
+    if( number == 1 && c.st.empty() )
     {
-        if( number % 2 == 0 )
         c.st.push( a.st.top() );
         a.st.pop();
         a.number--; c.number++;
         cout << a.name << " -> " << c.name << endl;
-        b.st.push( a.st.top() );
-        a.st.pop();
-        a.number--; b.number++;
-        cout << a.name << " -> " << b.name << endl;
+    }
+   else
+   {
+    if( b.st.empty() && c.st.empty() )//first move
+    {
+        if( a.st.top() % 2 == 0 )
+        {
+            b.st.push( a.st.top() );
+            a.st.pop();
+            a.number--; b.number++;
+            cout << a.name << " -> " << b.name << endl;
+        }
+        else
+        {
+            c.st.push( a.st.top() );
+            a.st.pop();
+            a.number--; c.number++;
+            cout << a.name << " -> " << c.name << endl;
+        }
+    }
+    else if( c.st.empty() && !b.st.empty() )//second move
+    {
+            c.st.push( a.st.top() );
+            a.st.pop();
+            a.number--; c.number++;
+            cout << a.name << " -> " << c.name << endl;
+    }
+    else if( !c.st.empty() && b.st.empty() )
+    {
+            b.st.push( a.st.top() );
+            a.st.pop();
+            a.number--; b.number++;
+            cout << a.name << " -> " << b.name << endl;
+    }
+    else if ( !c.st.empty() && !b.st.empty() )
+    {
+        if( b.st.top() < c.st.top() )
+        {
+            c.st.push( b.st.top() );
+            b.st.pop();
+            b.number--; c.number++;
+            cout << a.name << " -> " << c.name << endl;
+        }
+    }
+    }
     }
 }
