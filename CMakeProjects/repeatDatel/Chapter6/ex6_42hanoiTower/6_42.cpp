aa4ee41bd662330884a22d76b08438265bb05eb7
iaa2021 <ivanovandrey45@gmail.com>
2022-11-30 19:17:01 +0200

    handling ex6.42 khanoi tower

--- CMakeProjects/repeatDatel/Chapter6/ex6_42hanoiTower/6_42.cpp
+++ CMakeProjects/repeatDatel/Chapter6/ex6_42hanoiTower/6_42.cpp
@@ -9,46 +9,57 @@ struct peg
     stack <int> st;
     int number;
     char name;
+    void popTale()
+    {
+        st.pop(); number--;
+    }
+    void pushTale( int top )
+    {
+        st.push( top ); number++;
+    }
 };
-void hanoi( peg, peg, peg, int );
+peg *hanoi( peg *, int, int );
 int main()
 {
-    peg a; a.name = 'a'; a.number = 0;// a - primary store
-    peg b; b.name = 'b'; b.number = 0;//b - temporary store
-    peg c; c.name = 'c'; c.number = 0;//c - ultimate store
+    peg array[ 3 ];
+    array[ 0 ].name = 'a'; array[ 0 ].number = 0;// array[0] - primary store
+    array[ 1 ].name = 'b'; array[ 1 ].number = 0;// array[1] - temporary store
+    array[ 2 ].name = 'c'; array[ 2 ].number = 0;// array[2] - ultimate store
     int n, odd;//tale's number, odd number
     cout << "Input tail's number: ";
     cin >> n;
     for (int i = n; i >= 1; i--)
     {
-        a.st.push( i );
-        a.number++;
+        array[ 0 ].st.push( i );
+        array[ 0 ].number++;
     }
     if ( n % 2 == 0 )
-    {
         odd = 1;
-    }
     else
     odd = 0;
-    hanoi( a, b, c, odd );
-    cout << "Tail's number in a stack after one move is: " << a.number << endl;
-    cout << "in b stask is: " << b.number << endl;
-    cout << "in c stask is: " << c.number << endl;
+
+    cout << "\nOdd is " << odd << endl;
+    hanoi( array, odd, n );
+    cout << "Tail's number in a stack after one move is: " << array[ 0 ].number << endl;
+    cout << "in b stask is: " << array[ 1 ].number << endl;
+    cout << "in c stask is: " << array[ 2 ].number << endl;
+    cout << "general number is " << n << endl;
     return 0;
 }
-void hanoi( peg a, peg b, peg c, int odd )
+peg *hanoi( peg *array, int odd, int number )
 {
-    if( b.st.empty() && c.st.empty() )
+    if( array[ 1 ].st.empty() || array[ 2 ].st.empty() )
     {
-        if( ( a.st.top() + odd ) % 2 == 0 )
+        if( ( array[ 0 ].st.top() + odd ) % 2 == 0 )
         {
-            b.st.push( a.st.top() ); a.st.pop(); a.number--; b.number++;
-            cout << a.name << " -> " << b.name << endl;
+            array[ 1 ].pushTale( array[ 0 ].st.top() ); array[ 0 ].popTale(); 
+            cout << array[ 0 ].name << " -> " << array[ 1 ].name << endl; number--;
         }
-        else
+        else if( ( array[ 0 ].st.top() + odd ) % 2 != 0  )
         {
-            c.st.push( a.st.top() ); a.st.pop(); a.number--; c.number++;
-            cout << a.name << " -> " << c.name << endl;
+            array[ 2 ].pushTale( array[ 0 ].st.top() ); array[ 0 ].popTale(); 
+            cout << array[ 0 ].name << " -> " << array[ 2 ].name << endl; number--;
         }
     }
+    else return hanoi( array, odd, array[ 0 ].number-- );
 }
