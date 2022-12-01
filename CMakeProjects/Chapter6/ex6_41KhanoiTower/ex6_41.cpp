3fda262efc95b9f967e5fb0c693b79036dafa452
iaa2021 <ivanovandrey45@gmail.com>
2022-12-01 17:17:44 +0200

    handling ex 6.41, khanoi tower

--- CMakeProjects/Chapter6/ex6_41KhanoiTower/ex6_41.cpp
+++ CMakeProjects/Chapter6/ex6_41KhanoiTower/ex6_41.cpp
@@ -12,7 +12,7 @@ struct peg
 {
     stack <int>st;
     string name;
-    int number;
+    int number = 0;
 };
 void khanoi( int, int, peg, peg, peg );
 
@@ -20,7 +20,6 @@ int main()
 {
     peg a, b, c;
     a.name = "a"; b.name = "b"; c.name = "c";
-    a.number = 0; b.number = 0; c.number = 0;
     int number, odd;
     cout << "Input number: ";
     cin >> number;
@@ -45,54 +44,20 @@ int main()
 }
 void khanoi( int number, int odd, peg a, peg b, peg c )
 {
-            if( c.st.empty() && b.st.empty() )//move 1
-            {
-                if( ( a.st.top() + odd ) % 2 == 0 )
-                {
-                    b.st.push( a.st.top() ); a.st.pop(); a.number--; b.number++; number--;
-                    cout << a.name << " -> " << b.name << endl;
-                    c.st.push( a.st.top() ); a.st.pop(); a.number--; c.number++; number--;//2 move
-                    cout << a.name << " -> " << c.name << endl;
-                    c.st.push( b.st.top() ); b.st.pop(); b.number--; c.number++;//3 move
-                    cout << b.name << " -> " << c.name << endl;
-                }
-                else
-                {
-                    c.st.push( a.st.top() ); a.st.pop(); a.number--; c.number++; number--;
-                    cout << a.name << " -> " << c.name << endl;
-                    b.st.push( a.st.top() ); a.st.pop(); a.number--; b.number++; number--;//2 move
-                    cout << a.name << " -> " << b.name << endl;
-                    b.st.push( c.st.top() ); c.st.pop(); c.number--; b.number++;//3 move
-                    cout << c.name << " -> " << b.name << endl;
-                }
-            }
-            else if( ( a.st.top() + odd ) % 2 != 0 )
-            {
-                if( c.st.empty() || c.st.top() > a.st.top() )
-                {
-                    c.st.push( a.st.top() ); a.st.pop(); a.number--; c.number++; number--;
-                    cout << a.name << " -> " << c.name << endl;
-                    
-                }
-                
-            }
-            else if( ( ( a.st.top() + odd ) ) % 2 == 0 )
-            {
-                if( b.st.empty() || b.st.top() > a.st.top() )
-                {
-                    b.st.push( a.st.top() ); a.st.pop(); a.number--; b.number++; number--;
-                    cout << a.name << " -> " << b.name << endl;
-                   
-                }
-                 if( b.st.top() == 1 )
-                    {
-                        a.st.push( b.st.top() ); b.st.pop(); b.number--; a.number++; number++;//3 move
-                        cout << b.name << " -> " << a.name << endl;
-                    }
-            }
+    if( ( a.st.top() + odd ) % 2 != 0 && c.st.empty() )
+    {
+        c.st.push( a.st.top() ); a.st.pop(); a.number--; c.number++; number--;
+        cout << a.name << " -> " << c.name << endl;
+    }   
+    else if( ( a.st.top() + odd ) % 2 == 0 && b.st.empty() )
+    {
+        b.st.push( a.st.top() ); a.st.pop(); a.number--; b.number++; number--;
+        cout << a.name << " -> " << b.name << endl;
+    }
+    
 
-            else if( b.number > 0 )
-            number += b.number;
-           
+    else if( b.number > 0 )
+    number += b.number;
+
     khanoi( number, odd, a, b, c );
 }
