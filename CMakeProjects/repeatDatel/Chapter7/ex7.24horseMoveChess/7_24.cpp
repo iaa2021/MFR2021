fc1f0f2f05720cba5429a7149fb7b188149f98ed
iaa2021 <ivanovandrey45@gmail.com>
2022-12-13 16:34:12 +0200

    ex7.24 is handling

---
+++ CMakeProjects/repeatDatel/Chapter7/ex7.24horseMoveChess/7_24.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+void access( int[ 8 ][ 8 ], int[ 8 ][ 8 ] );
+int main()
+{
+    int accessibility[ 8 ][ 8 ] = { 0 };
+    int desk[ 8 ][ 8 ] = { 0 };
+    int horizontal[ 8 ] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+    int vertical[ 8 ] =  { -1, -2, -2, -1, 1, 2, 2, 1 };
+    int currentRow, currentColumn, count = 1;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if( desk[ i ][ j ] != 0 )
+            accessibility[ i ][ j ] = -1;
+            else
+            {
+                for (int k = 0; k < 8; k++)
+                {
+                    currentRow = i + vertical[ k ];
+                    currentColumn = j + horizontal[ k ];
+                    if( currentColumn < 8 && currentRow < 8 && currentColumn >= 0 && currentRow >= 0 && accessibility[ currentRow ][ currentColumn ] != -1 )
+                    accessibility[ i ][ j ]++;
+                }
+            }
+        }
+    }
+    cout << "Primary accessibility for horse move for chess desk is:\n";
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            cout << accessibility[ i ][ j ] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
+void access( int desk[ 8 ][ 8 ], int acc[ 8 ][ 8 ] )
+{
+
+}
---
+++ CMakeProjects/repeatDatel/Chapter7/ex7.24horseMoveChess/7_24.d
@@ -0,0 +1,2 @@
+obj/7_24.o: \
+ /home/iaa/Desktop/gitStore/MFR2021/CMakeProjects/repeatDatel/Chapter7/ex7.24horseMoveChess/7_24.cpp
