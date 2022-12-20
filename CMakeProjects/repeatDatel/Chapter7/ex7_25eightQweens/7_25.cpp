f7a4042a07f403872b65294501cc5bf08d86bebc
iaa2021 <ivanovandrey45@gmail.com>
2022-12-20 18:47:12 +0200

    ex.7.25 eight qweens

---
+++ CMakeProjects/repeatDatel/Chapter7/ex7_25eightQweens/7_25.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <iomanip>
+using std::setw;
+void access( int [ 8 ][ 8 ] );
+void print( int [ 8 ][ 8 ] );
+int main()
+{
+    int desk[ 8 ][ 8 ] = { 0 };
+    int accessibility[ 8 ][ 8 ] = { 0 };
+    access( accessibility );
+    print( accessibility );
+    cout << "\nPrimary accessibility is:\n";
+    int count = 1, row, column;
+    cout << "Input initial row, than column:\n";//Plasing first qween
+    cin >> row >> column;
+    desk[ row ][ column ] = count;
+    count++;
+    cout << "\nFirst move is:\n";
+    print( desk );
+    return 0;
+}
+void access( int acc[ 8 ][ 8 ] )
+{
+    int count = 15;
+    for ( int i = 0; i < 8; i++ )
+    {
+        for ( int j = 0; j < 8; j++ )
+        {
+            for (int k = 0; k < j; k++)//segment before j up & down
+            {
+                if( i - j + k >= 0 )
+                count++;
+                if( i + j - k < 8 )
+                count++;
+            }
+            for (int k = j + 1; k < 8; k++)//segment after j up & down
+            {
+                if( i - k + j >= 0 )
+                count++;
+                if( i + k - j < 8 )
+                count++;
+            }
+            acc[ i ][ j ] = count;
+            count = 15;
+        }
+    }
+}
+void print( int acc[ 8 ][ 8 ] )
+{
+    for ( int i = 0; i < 8; i++ )
+    {
+        for ( int j = 0; j < 8; j++ )
+        {
+            cout << setw( 2 ) << acc[ i ][ j ] << " ";
+        }
+        cout << endl;
+    }
+}
---
+++ CMakeProjects/repeatDatel/Chapter7/ex7_25eightQweens/7_25.d
@@ -0,0 +1,2 @@
+obj/7_25.o: \
+ /home/iaa/Desktop/gitStore/MFR2021/CMakeProjects/repeatDatel/Chapter7/ex7_25eightQweens/7_25.cpp
