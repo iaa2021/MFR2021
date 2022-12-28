6d6f2b84cc1ee5a4b4a475f98a20ea08aacffa75
iaa2021 <ivanovandrey45@gmail.com>
2022-12-28 19:55:37 +0200

    ex8.25 labirinth

--- CMakeProjects/Chapter8/8_25labirint/8_25.cpp
+++ CMakeProjects/Chapter8/8_25labirint/8_25.cpp
@@ -2,6 +2,10 @@
 using std::cout;
 using std::cin;
 using std::endl;
+#include <iomanip>
+using std::setw;
+template< class T >
+void print( T [ 12 ][ 12] );
 int main()
 {
     char lab[ 12 ][ 12 ] = {
@@ -18,13 +22,59 @@ int main()
         { '#', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '#' },
         { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' }
     };
+    print( lab );
+    int intLab[ 12 ][ 12 ] = { 0 };
     for (int i = 0; i < 12; i++)
     {
         for (int j = 0; j < 12; j++)
         {
-            cout << lab[ i ][ j ] << ' ';
+            if( lab[ i ][ j ] == '#' )
+            intLab[ i ][ j ] = 10;
+            else
+            intLab[ i ][ j ] = 0;
+        }
+    };
+    int curRow = 2, curColumn = 0;
+    lab[ curRow ][ curColumn ] = '0';
+    intLab[ curRow ][ curColumn ] = 1;
+    cout << "First move \n";
+    print( lab ); print( intLab );
+    int horisontal[ 4 ] = { -1, 0, 0, 1 }, vertical[ 4 ] = {  0, -1, 1, 0 };//west, nord, south, east
+    int hCorrection[ 4 ] = { 0, 1, -1, 0 }, vCorrection[ 4 ] = { -1, 0, 0, 1 };// right hand's correction
+    int cR = curRow, cC = curColumn, count = 1;
+    for (int a = 0; a < 10; a++)
+    {
+        lab[ curRow ][ curColumn ] = '.';
+        for (int i = 0; i < 4; i++)
+        {
+            cR += vertical[ i ]; cC += horisontal[ i ];
+            if( lab[ cR ][ cC ] == '.' && intLab[ cR ][ cC ] == 0 )
+            {
+                curRow += vertical[ i ]; curColumn += horisontal[ i ];
+                lab[ curRow ][ curColumn ] = '0';
+                intLab[ curRow ][ curColumn ] = 1;
+                count++;
+            }
+            else
+            {
+                cR = curRow; cC = curColumn;
+            }
+        }
+        cout <<  count << " move is \n";
+        print( lab ); print( intLab );
+    }
+    return 0;
+}
+template< class T >
+void print ( T lab[ 12 ][ 12 ] )
+{
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            cout << setw( 2 ) << lab[ i ][ j ] << ' ';
         }
         cout << endl;
     }
-    return 0;
+    cout << endl;
 }
