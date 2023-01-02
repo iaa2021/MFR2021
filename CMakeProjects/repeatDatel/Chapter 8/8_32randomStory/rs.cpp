407e1b98fa8e486550473f7c13309bcb35e1bf8b
iaa2021 <ivanovandrey45@gmail.com>
2023-01-02 18:33:24 +0200

    ex8.34 pig's latin  complete

--- CMakeProjects/repeatDatel/Chapter 8/8_32randomStory/rs.cpp
+++ CMakeProjects/repeatDatel/Chapter 8/8_32randomStory/rs.cpp
@@ -4,11 +4,19 @@ using std::cin;
 using std::endl;
 #include <string>
 using std::string;
+#include <string>
+using std::string;
+
+#include <cstring>
+using std::strcpy;
+using std::strtok;
 #include <cstdlib>
 using std::rand;
 using std::srand;
 #include <ctime>
 using std::time;
+#include <vector>
+using std::vector;
 #include "config.h"
 int main()
 {
@@ -36,5 +44,29 @@ int main()
         story.append( noun[ rand()%a ] ); story += ".\n";
     }
     cout << story << endl;
+    cout << "\nNow completing exersise 8.34.\n";
+    vector< string > story3;//storing a tokens
+    string story4;//resulting string
+    char str [ story.length() ];
+    int s = story.length();
+    strcpy( str, story.data() );
+    char *ptr = strtok( str, " ,.'\n'" );
+    while ( ptr != NULL )
+    {
+        story3.push_back( ptr );
+        ptr = strtok( NULL, " ,.'\n'" );
+    }
+    for (size_t i = 0; i < story3.size(); i++)
+    {
+        char s = story3.at( i ).at( 0 );
+        story3.at( i ) += s;
+        story3.at( i ).append( "ay" );
+        story3.at( i ).erase( 0, 1 );
+        story4.append( " ");
+        story4.append( story3.at( i ) );
+        if( ( i + 1 ) % 6 == 0 )
+        story4.append( ".\n" );
+    }
+    cout << endl << story4 << endl;
     return 0;
 }
