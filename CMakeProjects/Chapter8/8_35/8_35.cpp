a19d37893934bf22a70d413aa1a9342913af211e
iaa2021 <ivanovandrey45@gmail.com>
2023-01-03 17:46:15 +0200

    begin handling ex8.35

---
+++ CMakeProjects/Chapter8/8_35/8_35.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+#include <cstring>
+using std::strtok;
+int main()
+{
+    cout << "Hello, world.\n";
+    return 0;
+}
