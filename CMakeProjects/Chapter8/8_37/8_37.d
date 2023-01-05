b4625ec09b25b6dc0a701890d062915de8aab5e3
iaa2021 <ivanovandrey45@gmail.com>
2023-01-05 17:58:23 +0200

    ex8.37 with make

--- CMakeProjects/Chapter8/8_37/8_37.d
+++ CMakeProjects/Chapter8/8_37/8_37.d
@@ -1,2 +1,3 @@
 obj/8_37.o: \
- C:\Users\ivano\Desktop\gitStore\MFR2021\CMakeProjects\Chapter8\8_37/8_37.cpp
+ C:\Users\ivano\Desktop\gitStore\MFR2021\CMakeProjects\Chapter8\8_37/8_37.cpp \
+ C:\Users\ivano\Desktop\gitStore\MFR2021\CMakeProjects\Chapter8\8_37/config.h
