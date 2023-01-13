c8d7269760fd3669ae36fb46c9c1652d48630385
iaa2021 <ivanovandrey45@gmail.com>
2023-01-13 19:48:02 +0200

    ex9.12 is handling

---
+++ CMakeProjects/Chapter9/ex9_12/Rectangle/lib/Rectangle.cpp
@@ -0,0 +1,27 @@
+﻿#include "Rectangle.h"
+
+Rectangle::Rectangle( int array[2][4])
+{
+	cout << "Input rectangle's coordinats, high, than width of each point:\n";
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			cin >> array[i][j];
+		}
+	}
+	setRectangle(int array[2][4]);
+}
+
+bool Rectangle::setRectangle(int array[2][4])
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			if (array[i][j] < 0 || array[i][j] > 19)
+				return false;
+		}
+	}
+
+}
---
+++ CMakeProjects/Chapter9/ex9_12/Rectangle/lib/Rectangle.h
@@ -0,0 +1,25 @@
+﻿// Rectangle.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+class Rectangle
+{
+public:
+	Rectangle(int[2][4]);
+	bool setRectangle(int[2][4]);
+	int getWidth(int[2][4]);
+	int getLength(int[2][4]);
+	int area(int, int);
+	int perimeter(int, int);
+private:
+	int coordinats[2][4];
+	char field[20][20];
+	bool rectangle;
+	int length, width;
+};
+
