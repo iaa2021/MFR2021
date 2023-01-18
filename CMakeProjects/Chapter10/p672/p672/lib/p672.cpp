ebdd1c3c794428569ebac8410486629a6a06de3d
iaa2021 <ivanovandrey45@gmail.com>
2023-01-18 14:51:12 +0200

    p672 static function and static variable completed

---
+++ CMakeProjects/Chapter10/p672/p672/lib/p672.cpp
@@ -0,0 +1,47 @@
+﻿#include "p672.h"
+#include "config.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <cstring>
+using std::strlen;
+using std::strcpy;
+int Employee::count = 0;
+Employee::Employee(const char*first, const char*last)
+{
+	firstName = new char[strlen(first) + 1];
+	strcpy(firstName, first);
+	lastName = new char[strlen(last) + 1];
+	strcpy(lastName, last);
+	count++;
+	cout << "Constructor for " << firstName << "  " << lastName << " called.\n";
+}
+
+Employee::~Employee()
+{
+	cout << "Desstructor for " << firstName << "  " << lastName << " called.\n";
+	count--;
+	delete [] firstName;
+	delete [] lastName;
+}
+
+int Employee::getCount()
+{
+	return count;
+}
+
+const char* Employee::getFirstName() const
+{
+	return firstName;
+}
+
+const char* Employee::getLastName() const
+{
+	return lastName;
+}
+
+void Employee::getVersion() const
+{
+	cout << "\nLibrary version is: " << (PROJECT_VERSION_MAJOR) << '.';
+	cout << (PROJECT_VERSION_MINOR) << '.' << (PROJECT_VERSION_PATCH) << endl;
+}
