1090ad5dc1e81ea448b54ec03f96b48c8516319b
iaa2021 <ivanovandrey45@gmail.com>
2023-01-30 12:26:25 +0200

    processing ex704

--- CMakeProjects/repeatDatel/Chapter11/p704overloadArrays/lib/Array.cpp
+++ CMakeProjects/repeatDatel/Chapter11/p704overloadArrays/lib/Array.cpp
@@ -12,8 +12,48 @@ Array::Array( int arraySize )
 {
     size = ( arraySize > 0? arraySize: 10 );
     ptr = new int[ size ];
+    for (int i = 0; i < size; i++)
+    {
+        ptr[ i ] = 0;
+    }
 }
-
+Array::Array( const Array &arrayToCopy )
+    : size( arrayToCopy.size )
+{
+    for (int i = 0; i < size; i++)
+    {
+        ptr[ i ] = arrayToCopy.ptr[ i ];
+    }
+}
 Array::~Array()
 {
+    delete [] ptr;
+}
+const Array & Array::operator=( const Array &right )
+{
+    if( &right != this )// to avoid self assignment
+    {
+        if( size != right.size )
+        {
+            delete ptr;
+            size = right.size;
+            ptr = new int[ size ];
+        }
+        for (int i = 0; i < size; i++)
+        {
+            ptr[ i ] = right.ptr[ i ];
+        }
+    }
+    return *this;//allows to write a = b = c;
+}
+bool Array::operator==( const Array &right) const
+{
+    if( size != right.size )
+    return false;
+    for (int i = 0; i < size; i++)
+    {
+        if( ptr[ i ] != right.ptr[ i ] )
+        return false;
+    }
+    return true;
 }
