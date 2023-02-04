b2b6bd6ac8c0e119e7009db84b6a504323926513
iaa2021 <ivanovandrey45@gmail.com>
2023-02-04 15:49:20 +0200

    p732 completed with MinGW Makefiles

---
+++ CMakeProjects/repeatDatel/Chapter11/p732/buildMGW/src/CMakeFiles/732.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ivano/Desktop/gitWatch/CMakeProjects/repeatDatel/Chapter11/p732/src/732.cpp" "src/CMakeFiles/732.dir/732.cpp.obj" "gcc" "src/CMakeFiles/732.dir/732.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/ivano/Desktop/gitWatch/CMakeProjects/repeatDatel/Chapter11/p732/buildMGW/lib/CMakeFiles/myLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeProjects/repeatDatel/Chapter11/p732/buildMGW/src/CMakeFiles/732.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "732.exe"
+  "732.exe.manifest"
+  "732.pdb"
+  "CMakeFiles/732.dir/732.cpp.obj"
+  "CMakeFiles/732.dir/732.cpp.obj.d"
+  "lib732.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/732.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
