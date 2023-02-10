4970f43fee2a2fa00fca1ef2e56e10ed9c5bd84e
iaa2021 <ivanovandrey45@gmail.com>
2023-02-10 16:07:06 +0200

    ex 11.15 is handling

---
+++ CMakeProjects/repeatDatel/Chapter11/ex11.15/buildMGW/src/CMakeFiles/11_15.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ivano/Desktop/gitWatch/CMakeProjects/repeatDatel/Chapter11/ex11.15/src/11_15.cpp" "src/CMakeFiles/11_15.dir/11_15.cpp.obj" "gcc" "src/CMakeFiles/11_15.dir/11_15.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/ivano/Desktop/gitWatch/CMakeProjects/repeatDatel/Chapter11/ex11.15/buildMGW/lib/CMakeFiles/myLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeProjects/repeatDatel/Chapter11/ex11.15/buildMGW/src/CMakeFiles/11_15.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "11_15.exe"
+  "11_15.exe.manifest"
+  "11_15.pdb"
+  "CMakeFiles/11_15.dir/11_15.cpp.obj"
+  "CMakeFiles/11_15.dir/11_15.cpp.obj.d"
+  "lib11_15.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11_15.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
