d78de25b6159d738d8eb7637492f724ea37e99e9
iaa2021 <ivanovandrey45@gmail.com>
2023-02-20 13:29:49 +0200

    ex13.10 with vs

---
+++ CMakeProjects/repeatDatel/Chapter13/Shape/Shape/Shape.cpp
@@ -0,0 +1,17 @@
+﻿#include "Shape.h"
+
+Shape::Shape( double l, string n )
+	: length( l >= 0? l : 0 )
+{
+}
+
+void Shape::print() const
+{
+	cout << name << "'s length is: " << length << ", ";
+}
+
+void Shape::getVersion()
+{
+	cout << "Base shape's library version is: " << (PROJECT_VERSION_MAJOR) << '.';
+	cout << (PROJECT_VERSION_MINOR) << '.' << (PROJECT_VERSION_PATCH) << endl;
+}
---
+++ CMakeProjects/repeatDatel/Chapter13/Shape/Shape/Shape.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+class Shape
+{
+public:
+	Shape( double, string );
+	virtual double getArea() = 0;
+	virtual double getArea() = 0;
+	virtual void print() const;
+	static void getVersion();
+private:
+	double length;
+	string name;
+};
+
