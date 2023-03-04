6ec87292920ee40485acb50d3f459b314c4ee632
iaa2021 <ivanovandrey45@gmail.com>
2023-03-04 19:07:13 +0200

    p966 completed

--- CMakeProjects/repeatDatel/Chapter16/p966/p966.cpp
+++ CMakeProjects/repeatDatel/Chapter16/p966/p966.cpp
@@ -5,5 +5,30 @@ using std::endl;
 using std::runtime_error;
 void function3() throw( runtime_error )
 {
-    cout << "In function3"
+    cout << "In function3\n";
+    throw runtime_error( "runtime_error in function3" ); 
+}
+void function2() throw( runtime_error )
+{
+    cout << "function3 is called inside function2.\n";
+    function3();
+}
+void function1() throw( runtime_error )
+{
+    cout << "function2 is called inside function1.\n";
+    function2();
+}
+int main()
+{
+    try
+    {
+        cout << "function1 is called inside main:\n";
+        function1();
+    }
+    catch( runtime_error &error )
+    {
+        cout << "exeption occured: " << error.what() << endl;
+        cout << "exeption handled in main.\n";
+    }
+    return 0;
 }
---
+++ CMakeProjects/repeatDatel/Chapter16/p966/p966.d
@@ -0,0 +1,2 @@
+obj/p966.o: \
+ C:\Users\ivano\Desktop\gitWatch\CMakeProjects\repeatDatel\Chapter16\p966/p966.cpp
