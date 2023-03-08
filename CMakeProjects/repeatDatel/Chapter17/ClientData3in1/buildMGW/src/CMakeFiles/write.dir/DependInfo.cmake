25c9b01e14135ac73b060c271b13840d0ed94299
iaa2021 <ivanovandrey45@gmail.com>
2023-03-08 19:51:57 +0200

    client data 3 in 1 with minGW Makefiles

---
+++ CMakeProjects/repeatDatel/Chapter17/ClientData3in1/buildMGW/src/CMakeFiles/write.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ivano/Desktop/gitWatch/CMakeProjects/repeatDatel/Chapter17/ClientData3in1/src/write.cpp" "src/CMakeFiles/write.dir/write.cpp.obj" "gcc" "src/CMakeFiles/write.dir/write.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/ivano/Desktop/gitWatch/CMakeProjects/repeatDatel/Chapter17/ClientData3in1/buildMGW/lib/CMakeFiles/myLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeProjects/repeatDatel/Chapter17/ClientData3in1/buildMGW/src/CMakeFiles/write.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/write.dir/write.cpp.obj"
+  "CMakeFiles/write.dir/write.cpp.obj.d"
+  "libwrite.dll.a"
+  "write.exe"
+  "write.exe.manifest"
+  "write.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/write.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
