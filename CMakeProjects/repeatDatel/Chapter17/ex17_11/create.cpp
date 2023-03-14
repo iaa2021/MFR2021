7d366eda7731a64e1d0459c54da9d442940010e3
iaa2021 <ivanovandrey45@gmail.com>
2023-03-14 19:07:55 +0200

    ex17.11 finished

---
+++ CMakeProjects/repeatDatel/Chapter17/ex17_11/create.cpp
@@ -0,0 +1,6 @@
+#include "17_11.cpp"
+int main1()
+{
+    cout << "Hello, world.\n";
+    return 0;
+}
---
+++ CMakeProjects/repeatDatel/Chapter17/ex17_11/create.d
@@ -0,0 +1,3 @@
+obj/create.o: \
+ C:\Users\ivano\Desktop\gitWatch\CMakeProjects\repeatDatel\Chapter17\ex17_11/create.cpp \
+ C:\Users\ivano\Desktop\gitWatch\CMakeProjects\repeatDatel\Chapter17\ex17_11/17_11.cpp
