8683318db2fd7e9e69f3e158b78f1d9218907c62
iaa2021 <ivanovandrey45@gmail.com>
2023-03-16 19:56:50 +0200

    p1047 string erase, replace

---
+++ CMakeProjects/repeatDatel/Chapter18/p1047replace_erase/1047.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+int main()
+{
+    string string1( "The values in any left subtree"
+                    "\nare less than the value in the"
+                    "\nparent node and the values in"
+                    "\nany right subtree are greater"
+                    "\nthan the value in the parent node" );
+    cout << "Original string1 is:\n" << string1 << endl;
+    // to erase all symbols from 62 position
+    string1.erase( 62 );
+    cout << "\nString1 after erasing till 62 position is:\n" << string1 << endl;
+    cout << "\nString1 after first replacement is:\n";
+    size_t position = string1.find( " " );
+    while ( position != string::npos )
+    {
+        string1.replace( position, 1, ".");
+        position = string1.find( " ", position + 1 );
+    }
+    cout << string1 << endl;
+    cout << "\nString1 after second replacement is:\n";
+    position = string1.find( "." );
+    while ( position != string::npos )
+    {
+        string1.replace( position, 2, "xxxxx;;yyy", 5, 2 );
+        position = string1.find( ".", position + 1 );
+    }
+    cout << string1 << endl;
+    return 0;
+}
---
+++ CMakeProjects/repeatDatel/Chapter18/p1047replace_erase/1047.d
@@ -0,0 +1,2 @@
+obj/1047.o: \
+ /home/iaa/Desktop/gitStore/MFR2021/CMakeProjects/repeatDatel/Chapter18/p1047replace_erase/1047.cpp
