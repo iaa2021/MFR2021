1e58c898205d408e90e3058908ce410bae7bbe1d
iaa2021 <ivanovandrey45@gmail.com>
2023-03-18 17:10:13 +0200

    add work with iterators in p1049

--- CMakeProjects/repeatDatel/Chapter18/p1049insert/p1049.cpp
+++ CMakeProjects/repeatDatel/Chapter18/p1049insert/p1049.cpp
@@ -4,8 +4,11 @@ using std::cin;
 using std::endl;
 #include <string>
 using std::string;
+#include "config.h"
 int main()
 {
+    cout << "\nProject version is: " << (PROJECT_VERSION_MAJOR) << '.';
+    cout << (PROJECT_VERSION_MINOR) << '.' << (PROJECT_VERSION_PATCH)<< endl;
     string string1( "beginning end" );
     string string2( "middle " );
     string string3(  "2345678" );
@@ -22,5 +25,16 @@ int main()
     cout << "Strings after insert:\nstring1: " << string1;
     cout << "\nstring2: " << string2 << "\nstring3: " << string3;
     cout << "\nstring4: " << string4 << endl;
+    cout << "Example contents iterators.\n";
+    string1 = "Testing iterators";
+    string::const_iterator it = string1.begin();
+    cout << "string1 = " << string1;
+    cout << "\n(Using iterator it) string1 is: ";
+    while ( it != string1.end() )
+    {
+        cout << *it;
+        it++;
+    }
+    cout << endl;
     return 0;
 }
--- CMakeProjects/repeatDatel/Chapter18/p1049insert/p1049.d
+++ CMakeProjects/repeatDatel/Chapter18/p1049insert/p1049.d
@@ -1,2 +1,3 @@
 obj/p1049.o: \
- /home/iaa/Desktop/gitStore/MFR2021/CMakeProjects/repeatDatel/Chapter18/p1049insert/p1049.cpp
+ /home/iaa/Desktop/gitStore/MFR2021/CMakeProjects/repeatDatel/Chapter18/p1049insert/p1049.cpp \
+ /home/iaa/Desktop/gitStore/MFR2021/CMakeProjects/repeatDatel/Chapter18/p1049insert/config.h
