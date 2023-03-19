a9eef409c056fbb3c20e1030ef148f2a19c02f3f
iaa2021 <ivanovandrey45@gmail.com>
2023-03-19 12:39:07 +0200

    ex18.12 gibbet crossword

---
+++ CMakeProjects/repeatDatel/Chapter18/ex18_12/18_12.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+int main()
+{
+    string question = "hello"; int right = 0, count = 0; 
+    char letter;
+    string answer, result;
+    string gibbet[ 7 ] = { " 0 \n", "/", "|", ")\n", " |\n", "/", " )" };
+    cout << "Guess the word XXXXX, by input letters:\n";
+    cin >> letter;
+    while ( count < 8 )
+    {
+        if( letter == question.at( right ) )
+        {
+            answer += letter;
+            right++; count++;
+            if ( answer == question )
+            {
+                cout << "Well done, the word is: " << answer << endl;;
+                break;
+            }
+            else
+            cout << "\nInput next letter:\n? ";
+        }
+        else
+        {
+            result += gibbet[ count ];
+            cout << result << endl; count++;
+            if( count == 7 && answer != question )
+            {
+                cout << "You've missed.\n";
+                break;
+            }
+            else
+           { 
+                cout << "You've done " << count << " attempts.\n";
+                cout << "Try again:\n? ";
+            }
+        }
+        cin >> letter;
+    }
+    return 0;
+}
---
+++ CMakeProjects/repeatDatel/Chapter18/ex18_12/18_12.d
@@ -0,0 +1,2 @@
+obj/18_12.o: \
+ C:\Users\ivano\Desktop\gitWatch\CMakeProjects\repeatDatel\Chapter18\ex18_12/18_12.cpp
