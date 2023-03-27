bfcece13e53eec58daf48291e50abe1726bf4317
iaa2021 <ivanovandrey45@gmail.com>
2023-03-27 19:03:07 +0300

    list node with mingw

---
+++ CMakeProjects/repeatDatel/Chapter20/p1095llistNode/buildMGW/src/CMakeFiles/list.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ivano/Desktop/gitWatch/CMakeProjects/repeatDatel/Chapter20/p1095llistNode/src/list.cpp" "src/CMakeFiles/list.dir/list.cpp.obj" "gcc" "src/CMakeFiles/list.dir/list.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/ivano/Desktop/gitWatch/CMakeProjects/repeatDatel/Chapter20/p1095llistNode/buildMGW/lib/CMakeFiles/myLib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
