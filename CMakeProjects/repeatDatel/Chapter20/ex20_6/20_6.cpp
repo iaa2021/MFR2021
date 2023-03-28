a3bc13378669020c633371f4b3ad5ce879fe5ef6
iaa2021 <ivanovandrey45@gmail.com>
2023-03-28 18:23:59 +0300

    ex20.8

--- CMakeProjects/repeatDatel/Chapter20/ex20_6/20_6.cpp
+++ CMakeProjects/repeatDatel/Chapter20/ex20_6/20_6.cpp
@@ -2,8 +2,18 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using std::fixed;
+#include <cstdlib>
+using std::srand;
+using std::rand;
+#include <ctime>
+using std::time;
+#include <iomanip>
+using std::setw;
+using std::setprecision;
 struct ListNode
 {
+    friend class List;
     public:
     ListNode( int d )
     : data( d ), next( 0 ){}
@@ -11,11 +21,13 @@ struct ListNode
     {
         return data;
     }
+    private:
     ListNode *next;
     int data;
 };
 struct List
 {
+    friend class ListNode;
     public:
     List()
     : last( 0 ), first( 0 ){}
@@ -49,12 +61,35 @@ struct List
         }
         
     }
+    int sum()
+    {
+        ListNode *current = first; int value = 0;
+        while ( current != 0 )
+        {
+            value += current ->data;
+            current = current ->next;
+        }
+        return value;
+    }
+    double average()
+    {
+        double value; int count = 0;
+        ListNode *current = first; 
+        while ( current != 0 )
+        {
+            count++;
+            current = current ->next;
+        }
+        value = static_cast<double>( sum() ) / static_cast<double>( count );
+        return value;
+    }
     private:
     ListNode *last;
     ListNode *first;
 };
 int main()
 {
+    srand( time( 0 ) );
     List l1, l2;
     for (int i = 0; i < 6; i++)
     {
@@ -71,5 +106,17 @@ int main()
     l1.concatenate( l2 );
     cout << "\nAfter concatenation list1 is:\n";
     l1.print();
+    cout << "\nExersise 20.8\n";
+    List l3; int c;
+    for (size_t i = 0; i < 25; i++)
+    {
+        c = rand()%100 + 1;
+        l3.insert( c );
+    }
+    cout << "\nList3 is: ";
+    l3.print();
+    cout << "\nSum of list3's elements is: " << l3.sum() << endl;
+    cout << fixed << setprecision( 2 );
+    cout << "\nAverage of list3's elements is: " << l3.average() << endl;
     return 0;
 }
