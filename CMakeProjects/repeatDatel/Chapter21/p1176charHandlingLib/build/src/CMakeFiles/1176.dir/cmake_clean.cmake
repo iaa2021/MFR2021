5cb531b7364d742e3ab53486c62ad4854b8747e2
iaa2021 <ivanovandrey45@gmail.com>
2023-05-06 12:08:03 +0300

    p1176 char handling lib

---
+++ CMakeProjects/repeatDatel/Chapter21/p1176charHandlingLib/build/src/CMakeFiles/1176.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "1176"
+  "1176.pdb"
+  "CMakeFiles/1176.dir/1176.cpp.o"
+  "CMakeFiles/1176.dir/1176.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1176.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeProjects/repeatDatel/Chapter21/p1176charHandlingLib/src/1176.cpp
@@ -0,0 +1,32 @@
+#include "config.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <cctype>
+using std::isalnum;
+using std::isalpha;
+using std::isdigit;
+using std::isxdigit;
+int main()
+{
+    cout << "Project version is: " << (PROJECT_VERSION_MAJOR) << '.';
+    cout << (PROJECT_VERSION_MINOR) << '.' << (PROJECT_VERSION_PATCH) << endl;
+    cout << "According to isdigit:\n";
+    cout << ( isdigit( '8' )? "8 is a " : "8 isn't a " ) << "digit.\n";
+    cout << ( isdigit( '#' )? "# is a " : "# isn't a " ) << "digit.\n";
+    cout << "According to isalpha:\n";
+    cout << ( isalpha( 'A' )? "A is a " : "A isn't a " ) << "letter.\n";
+    cout << ( isalpha( 'b' )? "b is a " : "b isn't a " ) << "letter.\n";
+    cout << ( isalpha( '&' )? "& is a " : "& isn't a " ) << "letter.\n";
+    cout << ( isalpha( '7' )? "7 is a " : "7 isn't a " ) << "letter.\n";
+    cout << "According to isalnum:\n";
+    cout << ( isalnum( 'A' )? "A is a " : "A isn't a " ) << "digit or letter.\n";
+    cout << ( isalnum( 'b' )? "b is a " : "b isn't a " ) << "digit or letter.\n";
+    cout << ( isalnum( '#' )? "& is a " : "# isn't a " ) << "digit or letter.\n";
+    cout << ( isalnum( '7' )? "7 is a " : "7 isn't a " ) << "digit or letter.\n";
+    cout << "According to isxdigit:\n";
+    cout << ( isxdigit( 'F' )? "F is a " : "F isn't a " ) << "16x digit.\n";
+    cout << ( isxdigit( '#' )? "# is a " : "# isn't a " ) << "16x digit.\n";
+    
+    return 0;
+}
