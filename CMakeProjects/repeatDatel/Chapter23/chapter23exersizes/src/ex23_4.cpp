50dd106339a4d713f897f982c3f487c52afeab11
iaa2021 <ivanovandrey45@gmail.com>
2023-07-06 14:15:33 +0300

    ex23.4 part 2

--- CMakeProjects/repeatDatel/Chapter23/chapter23exersizes/src/ex23_4.cpp
+++ CMakeProjects/repeatDatel/Chapter23/chapter23exersizes/src/ex23_4.cpp
@@ -7,11 +7,13 @@ namespace Currency{
     const int ONE = 1; const int TWO = 2; const int THREE = 3; const int FIVE = 5;
     const int TEN = 10; const int FIFTY = 50; const int HUNDREED = 100;
 }
+using namespace Currency;
 int main()
 {
     cout << "Project version is " << (PROJECT_VERSION_MAJOR) << '.';
     cout << (PROJECT_VERSION_MINOR) << '.' << (PROJECT_VERSION_PATCH) << endl;
     cout << "\nExersise 23.4, namespace, const cast.\n";
-    cout << Currency::ONE << endl;
+    int a = *( const_cast< int * > ( &ONE )); 
+    cout << a << " a + 3 = " << (a += 3) << endl;
     return 0;
 }
